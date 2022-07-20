624cc7d0b06b10a68d8d7fe8153c75c682f69614
Russell Matney <russell.matney@gmail.com>
2022-07-20 15:59:25 -0400

    demo: godot main screen plugin tut
    https://docs.godotengine.org/en/stable/tutorials/plugins/editor/making_main_screen_plugins.html

---
+++ addons/demo_main_screen/HowdyButton.gd
@@ -0,0 +1,5 @@
+tool
+extends Button
+
+func _on_HowdyButton_pressed():
+  print("ahoy yerself, yee scalywag!")
---
+++ addons/demo_main_screen/plugin.gd
@@ -0,0 +1,33 @@
+tool
+extends EditorPlugin
+
+const MainPanel = preload("res://addons/demo_main_screen/MainPanel.tscn")
+
+var main_panel_instance
+
+func _enter_tree():
+  print("demo-main-screen entering tree")
+  main_panel_instance = MainPanel.instance()
+  # Add the main panel to the editor's main viewport.
+  get_editor_interface().get_editor_viewport().add_child(main_panel_instance)
+  # Hide the main panel. Very much required.
+  make_visible(false)
+
+func _exit_tree():
+  print("demo-main-screen exiting tree")
+  if main_panel_instance:
+    main_panel_instance.queue_free()
+
+func has_main_screen():
+  return true
+
+func make_visible(visible):
+  if main_panel_instance:
+    main_panel_instance.visible = visible
+
+func get_plugin_name():
+  return "Main Screen Plugin"
+
+func get_plugin_icon():
+  # Must return some kind of Texture for the icon.
+  return get_editor_interface().get_base_control().get_icon("Node", "EditorIcons")
