c86819484ddd0f27cafc51aeee0a7c8afd8ae5d5
Russell Matney <russell.matney@gmail.com>
2022-07-21 17:17:09 -0400

    wip: toying with OS and a gd shell_script

--- src/my_shell_script.gd
+++ src/my_shell_script.gd
@@ -1,8 +1,19 @@
-#!/usr/bin/env -S godot -s
-# NOTE running via emacs-gdscript-mode doesn't use this shebang
+#!/usr/bin/env -S godot -s --no-window
 extends SceneTree
 
 
 func _init():
+	print("args", OS.get_cmdline_args())
 	print("hello")
+	print("os can draw: ", OS.can_draw())
+	print("os props: ", OS.get_property_list())
+
+	# for p in OS.get_property_list():
+	# 	print(p.name)
+	# 	print("  " + p.name + ": ", OS.get(p.name))
+
+	print("window size: ", OS.get_window_size())
+	print("window position: ", OS.get_window_position())
+	print("no window: ", OS.get_window_position() == Vector2.ZERO)
+	# print("minimized: ", OS.window_minimized)
 	quit()
