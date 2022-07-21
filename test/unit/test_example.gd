df2f7b31c31145a124cd085e7adf31e2d0a2926e
Russell Matney <russell.matney@gmail.com>
2022-07-21 12:55:24 -0400

    feat: gut with no window, test-match helpers

--- test/unit/test_example.gd
+++ test/unit/test_example.gd
@@ -11,8 +11,8 @@ func before_all():
 func after_all():
   gut.p("ran run teardown", 2)
 
-# func test_assert_eq_number_not_equal():
-#   assert_eq(1, 2, "Should fail.  1 != 2")
+func test_assert_eq_number_not_equal():
+  assert_eq(1, 2, "Should fail.  1 != 2")
 
 func test_assert_eq_number_equal():
   assert_eq('asdf', 'asdf', "Should pass")
@@ -20,8 +20,8 @@ func test_assert_eq_number_equal():
 func test_assert_true_with_true():
   assert_true(true, "Should pass, true is true")
 
-# func test_assert_true_with_false():
-#   assert_true(false, "Should fail")
+func test_assert_true_with_false():
+  assert_true(false, "Should fail")
 
-# func test_something_else():
-#   assert_true(false, "didn't work")
+func test_something_else():
+  assert_true(false, "didn't work")
