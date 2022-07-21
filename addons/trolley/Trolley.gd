ad46482084668642acc53a4cb87bd2d29bc49ead
Russell Matney <russell.matney@gmail.com>
2022-07-20 20:20:55 -0400

    fix: troll -> trolley
    I like this name better.

---
+++ addons/trolley/Trolley.gd
@@ -0,0 +1,20 @@
+extends Node
+
+
+# returns a normalized Vector2 based on the controller's movement
+func move_dir():
+	var v_diff = Vector2()
+
+	if Input.is_action_pressed("move_right"):
+		v_diff.x += 1
+	if Input.is_action_pressed("move_left"):
+		v_diff.x -= 1
+	if Input.is_action_pressed("move_down"):
+		v_diff.y += 1
+	if Input.is_action_pressed("move_up"):
+		v_diff.y -= 1
+
+	return v_diff.normalized()
+
+func is_attack(event):
+	return event.is_action_pressed("attack")
---
+++ addons/trolley/plugin.gd
@@ -0,0 +1,12 @@
+tool
+extends EditorPlugin
+
+
+func _enter_tree():
+	print("trolley entering tree")
+	pass
+
+
+func _exit_tree():
+	print("trolley exiting tree")
+	pass
