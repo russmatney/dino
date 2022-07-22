57671ba16fd9eb25309658aa6c208c35332d37d8
Russell Matney <russell.matney@gmail.com>
2022-07-22 00:54:44 -0400

    feat: display state above players head
    Includes a few little machine fixes.
    
    Starting to see how this would be unit testable.
    Feels specific to _this_ state machine, but that's the process
    to provide tools for anyway.

--- addons/beehive/machine.gd
+++ addons/beehive/machine.gd
@@ -1,9 +1,8 @@
-# https://www.gdquest.com/tutorial/godot/design-patterns/finite-state-machine/
 class_name Machine
 extends Node
 
 export var initial_state := NodePath()
-onready var state: State = get_node(initial_state)
+var state: State
 
 ### ready #####################################################################
 
@@ -11,28 +10,41 @@ onready var state: State = get_node(initial_state)
 func _ready():
 	yield(owner, "ready")
 
+	if initial_state:
+		state = get_node(initial_state)
+
 	for child in get_children():
 		# assign machine to states
 		child.machine = self
-	state.enter()
+
+		# initial state defaults to first child
+		# (unless set via the inspector)
+		if not state:
+			state = child
+
+	if state:
+		state.enter()
 
 
 ### input #####################################################################
 
 
 func _unhandled_input(ev):
-	state.handle_input(ev)
+	if state:
+		state.handle_input(ev)
 
 
 ### process #####################################################################
 
 
 func _process(delta):
-	state.process(delta)
+	if state:
+		state.process(delta)
 
 
 func _physics_process(delta):
-	state.physics_process(delta)
+	if state:
+		state.physics_process(delta)
 
 
 ## transitions ###################################################################
--- test/unit/beehive/machine/Player.gd
+++ test/unit/beehive/machine/Player.gd
@@ -9,10 +9,20 @@ export(int) var gravity := 2000
 
 var initial_pos
 
+onready var state_label = $StateLabel
+onready var machine = $Machine
+
 func _ready():
     initial_pos = get_global_position()
+    machine.connect("transitioned", self, "on_transit")
+
+func on_transit(new_state):
+    set_state_label(new_state)
 
 func _process(_delta):
     if get_global_position().y > 30000:
         position = initial_pos
         velocity = Vector2.ZERO
+
+func set_state_label(label: String):
+    state_label.bbcode_text = "[center]" + label + "[/center]"
