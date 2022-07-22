fd37996b7c422c2ba7597ae86f57df8672296164
Russell Matney <russell.matney@gmail.com>
2022-07-22 16:39:48 -0400

    dino: basic mainMenu in place using NaviMenu

--- addons/navi/Navi.gd
+++ addons/navi/Navi.gd
@@ -3,6 +3,8 @@ extends Node
 
 var current_scene = null
 
+var last_scene_stack = []
+
 ###########################################################################
 # ready
 ###########################################################################
@@ -11,6 +13,9 @@ var current_scene = null
 func _ready():
 	var root = get_tree().get_root()
 	current_scene = root.get_child(root.get_child_count() - 1)
+
+	if "node_path" in current_scene:
+		last_scene_stack.push_back(current_scene.node_path)
 	print("[Navi] Current scene: ", current_scene)
 
 
@@ -20,6 +25,7 @@ func _ready():
 
 
 func nav_to(path):
+	last_scene_stack.push_back(path)
 	call_deferred("_deferred_goto_scene", path)
 
 
@@ -33,6 +39,7 @@ func _deferred_goto_scene(path):
 	# Instance the new scene.
 	current_scene = s.instance()
 	print("[Navi] Current scene: ", current_scene)
+	print("[Navi] last scene stack: ", last_scene_stack)
 
 	# Add it to the active scene, as child of root.
 	get_tree().get_root().add_child(current_scene)
--- src/menus/MainMenu.gd
+++ src/menus/MainMenu.gd
@@ -1,13 +1,23 @@
+tool
 extends NaviMenu
 
 var menu_scenes = [
 	{
 		"label": "Machine Movement Demo",
 		"nav_to": "res://test/unit/beehive/machine/MovementDemo.tscn",
-	}
+	},
+	{
+		"label": "Navi Menu Test",
+		"nav_to": "res://test/unit/navi/NaviMenuTest.tscn",
+	},
 ]
 
 
 func _ready():
 	for ms in menu_scenes:
 		add_menu_item(ms)
+
+	if Engine.editor_hint:
+		request_ready()
+
+	add_menu_item({"label": "I'm a disabled button"})
