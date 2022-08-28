89db6b732cd20ca7205668e8596c53b43358dfe4
Russell Matney <russell.matney@gmail.com>
2022-08-28 11:31:35 -0400

    feat: camera reparents itself to the found node
    May need to deal with 'owner' in this reparent helper, but we will wait
    and see for now.

--- src/autoloads/Util.gd
+++ src/autoloads/Util.gd
@@ -12,3 +12,9 @@ func _or(a, b = null, c = null, d = null, e = null):
 		return d
 	if e:
 		return e
+
+func reparent(child: Node, new_parent: Node):
+	# do we need to set owner?
+    var old_parent = child.get_parent()
+    old_parent.remove_child(child)
+    new_parent.add_child(child)
--- src/cameras/Camera.gd
+++ src/cameras/Camera.gd
@@ -8,20 +8,21 @@ func find_node_to_follow():
 	var nodes = get_tree().get_nodes_in_group(follow_group)
 
 	if nodes.size() > 1:
-		print("Camera found multiple nodes to follow", nodes)
+		print("[WARN] Camera found multiple nodes to follow", nodes)
 
 	if nodes.size() > 0:
 		following = nodes[0]
 		print("camera found player!", following)
+		Util.reparent(self, following)
 
 ###########################################################################
 
 func _ready():
 	if not following:
-	    find_node_to_follow()
+		find_node_to_follow()
 
 ###########################################################################
 
 func _process(_delta):
 	if not following:
-	    find_node_to_follow()
+		find_node_to_follow()
