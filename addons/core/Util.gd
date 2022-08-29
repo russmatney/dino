ce5fae9c943593a5af3b81892dab8da11d8fa4d0
Russell Matney <russell.matney@gmail.com>
2022-08-29 14:36:53 -0400

    feat: create 'core' addon with fonts and util
    Been looking for a reason to move this Util into an addon, and I'm not
    making a 'fonts' addon - 'core' will now bring in fonts and other useful
    core assets, plus utils and other things not warranting a full addon.

--- addons/core/Util.gd
+++ addons/core/Util.gd
@@ -13,13 +13,8 @@ func _or(a, b = null, c = null, d = null, e = null):
 	if e:
 		return e
 
-
 func reparent(child: Node, new_parent: Node):
 	# do we need to set owner?
-	var old_parent = child.get_parent()
-	old_parent.remove_child(child)
-	new_parent.add_child(child)
-
-
-func autoload_exists(autoload_name) -> bool:
-	return $"/root".has_node(autoload_name)
+    var old_parent = child.get_parent()
+    old_parent.remove_child(child)
+    new_parent.add_child(child)
