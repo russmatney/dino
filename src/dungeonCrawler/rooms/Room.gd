9cc6c33b70371ec85f663e9e00969905cdf3dfc8
Russell Matney <russell.matney@gmail.com>
2022-09-08 16:30:42 -0400

    feat: dungeon wall tiles, and reptile/maze clean up
    renames maze -> dungeon crawler.
    pulls some reptile assets into templates dir

---
+++ src/dungeonCrawler/rooms/Room.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+onready var player_start = get_node("%PlayerStart")
+var player
+
+# TODO config warning when no player_start
+
+func _ready():
+	player = player_start.spawn_player(self)
+
+	if player:
+		player.add_to_group("player")
+	else:
+		print("failed to spawn player?")
--- src/menus/MainMenu.gd
+++ src/menus/MainMenu.gd
@@ -11,8 +11,12 @@ var menu_scenes = [
 		"nav_to": "res://test/unit/navi/NaviMenuTest.tscn",
 	},
 	{
-		"label": "Maze Game",
-		"nav_to": "res://src/maze/Maze.tscn",
+		"label": "Maze",
+		"nav_to": "res://src/dungeonCrawler/rooms/Maze.tscn",
+	},
+	{
+		"label": "Room",
+		"nav_to": "res://src/dungeonCrawler/rooms/Maze.tscn",
 	},
 	{
 		"label": "Clawe Dashboard",
