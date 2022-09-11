000a0975574cbc2d6a48a676188a13c89cc92e76
Russell Matney <russell.matney@gmail.com>
2022-09-11 15:17:15 -0400

    feat: door lock/unlock actions, DoorTest scene
    Every scene could use a <scene>Test, couldn't it?
    
    Dino needs a better godot default_env.

--- src/dungeonCrawler/Door.gd
+++ src/dungeonCrawler/Door.gd
@@ -1,6 +1,6 @@
 extends StaticBody2D
 
-enum door_state {OPEN, CLOSED}
+enum door_state {LOCKED, OPEN, CLOSED}
 
 export(door_state) var state = door_state.CLOSED setget set_door_state
 
@@ -13,23 +13,27 @@ func set_door_state(val):
 
 	match state:
 		door_state.OPEN:
-			if anim:
-				anim.animation = "open"
-			if coll_shape:
-				coll_shape.set_disabled(true)
+			anim.animation = "open"
+			coll_shape.set_disabled(true)
 		door_state.CLOSED:
-			if anim:
-				anim.animation = "closed"
-			if coll_shape:
-				coll_shape.set_disabled(false)
-			else:
-				print("[WARN] no door collision shape")
+			anim.animation = "closed"
+			coll_shape.set_disabled(false)
+		door_state.LOCKED:
+			anim.animation = "closed" # TODO support "locked"
+			coll_shape.set_disabled(false)
 
 func open_door():
 	set_door_state(door_state.OPEN)
 
 func close_door():
 	set_door_state(door_state.CLOSED)
+
+func unlock_door():
+	set_door_state(door_state.CLOSED)
+
+func lock_door():
+	set_door_state(door_state.LOCKED)
+
 
 #######################################################################33
 # ready
@@ -44,6 +48,16 @@ func _ready():
 # actions
 
 var bodies = []
+
+var unlock_door_action = {
+	"func": funcref(self, "unlock_door"),
+	"label": "Unlock"
+	}
+
+var lock_door_action = {
+	"func": funcref(self, "lock_door"),
+	"label": "lock"
+	}
 
 var open_door_action = {
 	"func": funcref(self, "open_door"),
@@ -62,8 +76,12 @@ func _on_ActionArea_body_entered(body:Node):
 		match state:
 			door_state.OPEN:
 				body.add_action(close_door_action)
+				body.add_action(lock_door_action)
 			door_state.CLOSED:
 				body.add_action(open_door_action)
+				body.add_action(lock_door_action)
+			door_state.LOCKED:
+				body.add_action(unlock_door_action)
 
 
 func _on_ActionArea_body_exited(body:Node):
@@ -73,3 +91,5 @@ func _on_ActionArea_body_exited(body:Node):
 		# should no-op ok?
 		body.remove_action(close_door_action)
 		body.remove_action(open_door_action)
+		body.remove_action(lock_door_action)
+		body.remove_action(unlock_door_action)
