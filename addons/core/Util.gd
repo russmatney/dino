406df20b8653e60a45e5416cc9c83c86395d0787
Russell Matney <russell.matney@gmail.com>
2022-09-16 19:08:56 -0400

    fix: reparent in a deferred, in case the nodes isn't ready

--- addons/core/Util.gd
+++ addons/core/Util.gd
@@ -14,22 +14,25 @@ func _or(a, b = null, c = null, d = null, e = null):
 		return e
 
 func reparent(child: Node, new_parent: Node):
-	# do we need to set owner?
-    var old_parent = child.get_parent()
-    old_parent.remove_child(child)
-    new_parent.add_child(child)
+	call_deferred("do_reparent", child, new_parent)
+
+func do_reparent(child, new_parent):
+	# TODO need to set owner as well to support creating PackedScenes
+	var old_parent = child.get_parent()
+	old_parent.remove_child(child)
+	new_parent.add_child(child)
 
 # https://godotengine.org/qa/27869/how-to-get-the-nearest-object-in-a-group
 func nearest_node(source: Node, targets) -> Node:
-    # assume the first is closest
-    var nearest_target = targets[0]
+	# assume the first is closest
+	var nearest_target = targets[0]
 
-    # look through spawn nodes to see if any are closer
-    for target in targets:
-         if target.global_position.distance_to(source.global_position) < nearest_target.global_position.distance_to(source.global_position):
-            nearest_target = target
+	# look through spawn nodes to see if any are closer
+	for target in targets:
+		 if target.global_position.distance_to(source.global_position) < nearest_target.global_position.distance_to(source.global_position):
+			 nearest_target = target
 
-    return nearest_target
+	return nearest_target
 
 func get_first_child_in_group(node: Node, group_name: String) -> Node:
 	# only checks first children
