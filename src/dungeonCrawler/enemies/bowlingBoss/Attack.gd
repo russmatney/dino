9f91afdf71364626b373a28271b7d394a47bd079
Russell Matney <russell.matney@gmail.com>
2022-09-16 12:36:51 -0400

    wip: firing basic bowling ball in attack state

--- src/dungeonCrawler/enemies/bowlingBoss/Attack.gd
+++ src/dungeonCrawler/enemies/bowlingBoss/Attack.gd
@@ -4,7 +4,28 @@ extends State
 func enter(_msg = {}):
 	print("entering attack state for actor: ", actor)
 
+var once = true
 
 func process(_delta: float):
 	if actor.bodies.size() == 0:
 		machine.transit("Idle")
+		return
+
+	var target = actor.bodies[0]
+
+	# TODO tell before attacking, pause/move after
+	if once:
+		bowl_attack(target)
+		once = false
+
+onready var ball_scene = preload("res://src/dungeonCrawler/weapons/BowlingBall.tscn")
+var bowl_speed = 5
+
+func bowl_attack(_target):
+	var ball = ball_scene.instance()
+	ball.position = actor.get_global_position()
+	Navi.current_scene.call_deferred("add_child", ball)
+
+	# TODO angle toward target
+	var impulse_dir = Vector2(-200, 0)
+	ball.velocity = impulse_dir * bowl_speed
--- src/dungeonCrawler/weapons/BowlingBall.gd
+++ src/dungeonCrawler/weapons/BowlingBall.gd
@@ -1,10 +1,13 @@
 extends KinematicBody2D
 
+onready var light = $Light2D
+export(bool) var light_enabled = false
 
 var velocity = Vector2.ZERO
 
 func _ready():
 	velocity = Vector2(-250, 0)
+	light.enabled = light_enabled
 
 func _physics_process(delta):
 	var collision_info = move_and_collide(velocity * delta)
