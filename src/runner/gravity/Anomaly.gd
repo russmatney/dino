01e0ba24a234965bbbfecec5f2364f2e4a4c93b3
Russell Matney <russell.matney@gmail.com>
2022-11-12 21:46:15 -0500

    feat: some rigid blocks for the anomaly
    Unfortunately area2d physics overrides don't work on kinematic bodies :/
    
    Here's some blocks that get pushed around instead.

--- src/runner/gravity/Anomaly.gd
+++ src/runner/gravity/Anomaly.gd
@@ -3,11 +3,33 @@ extends RunnerRoom
 var max_runs = 3
 var runs = 0
 
+
+onready var block = $Block
+var block_pos
+onready var block_two = $Block2
+var block_two_pos
+
+
 func _ready():
 	Util.ensure_connection(self, "player_entered", self, "_on_player_entered")
 
+	block_pos = block.global_position
+	block_two_pos = block_two.global_position
+
+func reset():
+	block.position = block_pos
+	block_two.position = block_two_pos
+
+
 func _on_player_entered(player):
+	print("player entered")
+
+	reset()
+
 	runs = runs + 1
+
+
+
 
 func is_finished():
 	print(self, "is funished? runs: ", runs)
