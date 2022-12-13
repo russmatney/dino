9c61ba550643cd2e71df686c0b29264d841f18d3
Russell Matney <russell.matney@gmail.com>
2022-12-13 16:08:50 -0500

    feat: basic glowmba, some rearranging

---
+++ src/ghosts/enemies/Glowmba.gd
@@ -0,0 +1,22 @@
+extends KinematicBody2D
+
+var dead = false
+
+#######################################################################33
+# physics process
+
+var dir = Vector2.LEFT
+export(int) var speed = 200
+export(int) var gravity := 4000
+var velocity = dir * speed
+
+func _physics_process(delta):
+	velocity.y += gravity * delta
+
+	if not dead:
+		velocity.x = dir.x * max(speed, velocity.x)
+		velocity = move_and_slide(velocity, Vector2.UP)
+		if is_on_wall():
+			match dir:
+				Vector2.LEFT: dir = Vector2.RIGHT
+				Vector2.RIGHT: dir = Vector2.LEFT
