707d9fe86cf1318bccf864d28d0f06ac05b0d7f3
Russell Matney <russell.matney@gmail.com>
2022-12-12 22:09:38 -0500

    feat: tweaking hangingLight pinjoints
    Boost gravity on the lamp (50x), set softness on the first/last pin joint.
    
    I experimented with bias as well, but it makes things much springier.

--- src/ghosts/player/Player.gd
+++ src/ghosts/player/Player.gd
@@ -24,11 +24,9 @@ func shader_loop():
 	tween = get_tree().create_tween()
 	tween.set_loops(0)
 
-	tween.tween_property(anim.get_material(), "shader_param/ghost", 1.0, 1)
 	tween.parallel().tween_property(anim.get_material(), "shader_param/red_displacement", 1.0, 1)
 	tween.parallel().tween_property(anim.get_material(), "shader_param/blue_displacement", 1.0, 1)
 	tween.parallel().tween_property(anim.get_material(), "shader_param/green_displacement", 1.0, 1)
-	tween.tween_property(anim.get_material(), "shader_param/ghost", 0.0, 1)
 	tween.parallel().tween_property(anim.get_material(), "shader_param/red_displacement", -1.0, 1)
 	tween.parallel().tween_property(anim.get_material(), "shader_param/blue_displacement", -1.0, 1)
 	tween.parallel().tween_property(anim.get_material(), "shader_param/green_displacement", -1.0, 1)
