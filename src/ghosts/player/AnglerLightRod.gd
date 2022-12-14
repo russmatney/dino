80d1dabc5fd1e81267fac1ee801445cf07873c29
Russell Matney <russell.matney@gmail.com>
2022-12-14 18:49:15 -0500

    fix: update anglerLightRod position based on player.facing_direction
    Now sticking to the 'facing' side.

--- src/ghosts/player/AnglerLightRod.gd
+++ src/ghosts/player/AnglerLightRod.gd
@@ -1,18 +1,14 @@
 extends RigidBody2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 #var mouse_pos
 #var ang_to_mouse
-onready var player_node = .get_node("../Player")
+onready var player = .get_node("../Player")
 onready var handle_pinjoint = .get_node("../RodHandlePin")
 onready var player_machine = .get_node("../Player/Machine")
 
 onready var initial_pos = position
-onready var initial_x_rel = abs(player_node.position.x - position.x)
-onready var initial_y_rel = abs(player_node.position.y - position.y)
+onready var initial_x_rel = abs(player.position.x - position.x)
+onready var initial_y_rel = abs(player.position.y - position.y)
 #var colors = [Color(1.0, 0.0, 0.0, 1.0),
 #		  Color(0.0, 1.0, 0.0, 1.0),
 #		  Color(0.0, 0.0, 1.0, 0.0)]
@@ -25,19 +21,20 @@ func _ready():
 #func _input(event):
 #	randomize()
 #	modulate = colors[randi() % colors.size()]
-	
+
 
 func _process(_delta):
-	if player_node.velocity.y != 0:
-		position.y = player_node.position.y + initial_y_rel
-	if player_node.velocity.x > 0:
-		position.x = player_node.position.x + initial_x_rel
-	elif player_node.velocity.x < 0:
-		position.x = player_node.position.x - initial_x_rel
+	if player.velocity.y != 0:
+		position.y = player.position.y + initial_y_rel
+
+	match player.facing_direction:
+		player.DIR.right:
+			position.x = player.position.x + initial_x_rel
+		player.DIR.left:
+			position.x = player.position.x - initial_x_rel
+
 	handle_pinjoint.position = position
-		
-	
-	
+
 #func _integrate_forces(state):
 #	state.add_torque(global_position.angle_to_point(get_global_mouse_position()))
 	# state.add_torque(angular_damp)
