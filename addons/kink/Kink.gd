84df5d4affe1fb7c8d2933c3d840b4cb066f2e97
Russell Matney <russell.matney@gmail.com>
2022-12-13 20:53:14 -0500

    wip: kink register, current_message
    The funcs, signals, connects, etc in Glowmba.gd should be moved into a
    nicer api in Kink - probably assertions and connects in Kink.register()

--- addons/kink/Kink.gd
+++ addons/kink/Kink.gd
@@ -3,24 +3,52 @@ extends Node
 
 var ink_states = {}
 
+############################################################
+# current_message
+
 func current_message(node):
-	print("[KINK]: current_message for ", node.name)
 	if node.name in ink_states:
 		var state = ink_states[node.name]
-		print(state)
-		if "current_message" in state:
-			return state["current_message"]
+		if "ink_player" in state:
+			return state["ink_player"].get_current_text()
 		return str("No current_message for ", node.name)
 	return str("No ink_state found for ", node.name)
 
-func new_ink_state(node):
+############################################################
+# register
+
+func register(node):
+	# TODO unit tests:
+	#   - assert node passed in has usabel ink_player set afterwards
+	#   - assert warnings on missing or mis-typed kink_file (or w/e property)
+	#   - assert ink_states can be accessed as expected (msg, node_name)
+
+	print("[KINK]: initing new ink node")
+
 	ink_states[node.name] = {
 		"msg": "debug first msg",
 		"node_name": node.name,
 		}
+
+	if node.kink_file:
+		var ink_player = InkPlayer.new()
+		ink_player.ink_file = node.kink_file
+
+		# maybe we care about releasing this ref?
+		# TODO consider WeakRef ?
+		ink_states[node.name]["ink_player"] = ink_player
+
+		node.ink_player = ink_player
+	else:
+		print("[WARN]: kink node without kink file", node)
+
 	return ink_states[node.name]
 
-func kink_init(node):
-	print("[KINK]: initing new ink node")
+############################################################
+# ready
 
-	new_ink_state(node)
+func _ready():
+	if Engine.editor_hint:
+		request_ready()
+
+	print("Kink autoload ready")
--- src/ghosts/enemies/Glowmba.gd
+++ src/ghosts/enemies/Glowmba.gd
@@ -3,12 +3,59 @@ extends KinematicBody2D
 
 onready var kink_label = $KinkLabel
 
+export (Resource) var kink_file = preload("res://src/ghosts/enemies/glowmba.ink.json")
+
+var ink_player
+
 func _ready():
-	Kink.new_ink_state(self)
+	print("Glowmba ready")
+	Kink.register(self)
+
+	call_deferred("add_child", ink_player)
+
+	# TODO do this in register, with more warnings/validations
+	ink_player.connect("ready", self, "kink_player_ready")
+	ink_player.connect("loaded", self, "kink_loaded")
+	ink_player.connect("continued", self, "kink_continued")
+	ink_player.connect("prompt_choices", self, "kink_prompt_choices")
+	ink_player.connect("ended", self, "kink_ended")
+
+
+	if Engine.editor_hint:
+		request_ready()
+
+func kink_player_ready():
+	print("kink player ready")
+	ink_player.create_story()
+
+func kink_loaded(success):
+	print("Glowmba kink loaded, with success: ", success)
+	print("My ink story: ", ink_player._story)
+
+	ink_player.continue_story()
+
+func kink_continued(text, tags):
+	print("[KINK]: continued ", text, " tags: ", tags)
 
 	var msg = Kink.current_message(self)
+	print("msg: ", msg)
+
 	if msg:
 		kink_label.set_text(msg)
+
+	print("[KINK]: current text:")
+	print(ink_player.get_current_text())
+
+	# TODO call in Kink?
+	ink_player.continue_story()
+
+func kink_prompt_choices(choices):
+	print("[KINK]: choices", choices)
+
+	# TODO select a choice
+
+func kink_ended():
+	print("[KINK]: ended")
 
 #######################################################################33
 # physics process
