5e41e7581a6d5f9ab5fbf07643054684b4f2789c
Russell Matney <russell.matney@gmail.com>
2022-12-15 19:45:25 -0500

    feat: gloomba random hopping, drop ink integration
    Adds more gloombas to the library.

--- src/ghosts/enemies/Glowmba.gd
+++ src/ghosts/enemies/Glowmba.gd
@@ -9,21 +9,6 @@ var ink_player
 
 
 func _ready():
-	print("Glowmba ready")
-	Kink.register(self)
-
-	call_deferred("add_child", ink_player)
-
-	# TODO do this in register, with more warnings/validations
-	ink_player.connect("ready", self, "kink_player_ready")
-	ink_player.connect("loaded", self, "kink_loaded")
-	ink_player.connect("continued", self, "kink_continued")
-	ink_player.connect("prompt_choices", self, "kink_prompt_choices")
-	ink_player.connect("ended", self, "kink_ended")
-
-	if Engine.editor_hint:
-		request_ready()
-
 	match initial_dir:
 		DIR.left:
 			move_dir = Vector2.LEFT
@@ -36,49 +21,12 @@ func _ready():
 		Vector2.LEFT:
 			$AnimatedSprite.flip_h = false
 
+	speed_factor = (randi() % speed_range) - speed_range
 
-func kink_player_ready():
-	ink_player.create_story()
+	Ghosts.create_notification("Gloomba alive!")
 
-
-func kink_loaded(_success):
-	# print("Glowmba kink loaded, with success: ", success)
-	# print("My ink story: ", ink_player._story)
-
-	ink_player.continue_story()
-
-
-func kink_continued(_text, _tags):
-	# print("[KINK]: continued ", text, " tags: ", tags)
-
-	var msg = Kink.current_message(self)
-
-	if msg:
-		# TODO we probably want a smarter version of this
-		# e.g. handling sentences vs newlines
-		for m in msg.split("\n"):
-			if m: # skip empty lines
-				kink_label.set_text(m)
-				Ghosts.create_notification(m)
-				yield(get_tree().create_timer(2), "timeout")
-
-	# print("[KINK]: current text:")
-	# print(ink_player.get_current_text())
-
-	# TODO call in Kink?
-	ink_player.continue_story()
-
-
-func kink_prompt_choices(choices):
-	print("[KINK]: choices", choices)
-
-	# TODO select a choice
-
-
-func kink_ended():
-	pass
-	# print("[KINK]: ended")
-
+	if Engine.editor_hint:
+		request_ready()
 
 #######################################################################33
 # physics process
@@ -88,19 +36,28 @@ enum DIR { left, right }
 export(DIR) var initial_dir = DIR.left
 var move_dir = Vector2.RIGHT
 export(int) var speed = 100
+export(int) var speed_range = 90
 export(int) var gravity := 4000
+var speed_factor = 0
+
 var velocity = move_dir * speed
+
+export(int) var fly_speed := 1500
+export(int) var fly_range := 400
+var flying = false
 
 func _physics_process(delta):
 	if not Engine.editor_hint:
 		velocity.y += gravity * delta
 
 		if not dead:
-			velocity.x = move_dir.x * max(speed, velocity.x)
+			velocity.x = move_dir.x * max(speed + speed_factor, velocity.x)
 			velocity = move_and_slide(velocity, Vector2.UP)
 
 			if abs(velocity.x) > 0.1:
 				$AnimatedSprite.animation = "walk"
+			elif velocity.y < -0.1:
+				$AnimatedSprite.animation = "fly"
 			else:
 				$AnimatedSprite.animation = "idle"
 
@@ -112,3 +69,31 @@ func _physics_process(delta):
 					Vector2.RIGHT:
 						move_dir = Vector2.LEFT
 						$AnimatedSprite.flip_h = false
+
+func fly():
+	if not flying:
+		flying = true
+		velocity.y = -1 * (fly_speed + (randi() % fly_range) - (fly_range/3.0))
+		yield(get_tree().create_timer(1), "timeout")
+		flying = false
+
+
+#############################################################
+
+var enemies = []
+var player
+
+func _on_Detectbox_body_entered(body:Node):
+	if body.is_in_group("enemies"):
+		enemies.append(body)
+		fly()
+
+	if body.is_in_group("player"):
+		player = body
+		fly()
+
+func _on_Detectbox_body_exited(body:Node):
+	if body.is_in_group("enemies"):
+		enemies.erase(body)
+	if body.is_in_group("player"):
+		player = null
