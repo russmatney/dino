5b60b504a348078d0177b519da5008d17d295457
Russell Matney <russell.matney@gmail.com>
2023-01-07 14:53:02 -0500

    feat: seedbox and actions abstraction on player

---
+++ src/harvey/items/SeedBox.gd
@@ -0,0 +1,18 @@
+extends StaticBody2D
+
+export (String) var produce_type = "carrot"
+
+onready var produce_icon = $ProduceIcon
+
+func _ready():
+	produce_icon.animation = produce_type
+
+func _on_Detectbox_body_entered(body:Node):
+	if body.has_method("add_action") and body.has_method("pickup_seed"):
+		body.add_action({"obj": body, "method": "pickup_seed", "arg": produce_type})
+
+
+func _on_Detectbox_body_exited(body:Node):
+	if body.has_method("remove_action"):
+		# TODO maybe remove by method
+		body.remove_action({"obj": body, "method": "pickup_seed", "arg": produce_type})
--- src/harvey/player/Player.gd
+++ src/harvey/player/Player.gd
@@ -9,6 +9,13 @@ func _ready():
 	machine.connect("transitioned", self, "on_transit")
 
 ############################################################
+# _input
+
+func _unhandled_input(event):
+	if actions and Trolley.is_action(event):
+		perform_action()
+
+############################################################
 # machine
 
 onready var machine = $Machine
@@ -41,3 +48,41 @@ func face_left():
 	anim.flip_h = false
 
 ############################################################
+# actions
+
+onready var action_label = $ActionLabel
+var actions = {}
+
+func update_action_label():
+	if not actions:
+		action_label.bbcode_text = ""
+		return
+
+	if actions:
+		var ax = actions.values()[0]
+		action_label.bbcode_text = "[center]" + ax["method"].capitalize() + "[/center]"
+
+func add_action(ax):
+	actions[ax["method"]] = ax
+	update_action_label()
+
+func remove_action(ax):
+	actions.erase(ax["method"])
+	update_action_label()
+
+func perform_action():
+	if not actions:
+		return
+
+	var ax = actions.values()[0]
+	print("performing action: ", ax)
+	if ax["arg"]:
+		ax["obj"].call(ax["method"], ax["arg"])
+	else:
+		ax["obj"].call(ax["method"])
+
+############################################################
+# pickup seed
+
+func pickup_seed(produce_type):
+	print("player picking up seed type: ", produce_type)
