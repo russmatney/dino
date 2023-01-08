689930317bfa59655dd1fdcc170cc0ddaa920d97
Russell Matney <russell.matney@gmail.com>
2023-01-08 13:40:24 -0500

    feat: squash and stretch shader
    From this tutorial: https://github.com/PlayWithFurcifer/SpriteDeformation
    
    Adds a shader for deforming sprites, including some knockback. Adds it
    to the seed boxes.

--- src/harvey/items/SeedBox.gd
+++ src/harvey/items/SeedBox.gd
@@ -4,15 +4,60 @@ extends StaticBody2D
 export (String, "carrot", "tomato", "onion") var produce_type = "carrot"
 
 onready var produce_icon = $ProduceIcon
+onready var anim = $AnimatedSprite
+
+##########################################################
+# ready
 
 func _ready():
 	produce_icon.animation = produce_type
 
+##########################################################
+# input
+
+func _unhandled_input(event):
+	if event is InputEventMouseButton and event.is_pressed():
+		var direction = anim.global_position - get_global_mouse_position()
+		deform(direction)
+
+##########################################################
+# detectbox
+
 func _on_Detectbox_body_entered(body:Node):
 	if body.has_method("add_action") and body.has_method("pickup_seed"):
-		body.add_action({"obj": body, "method": "pickup_seed", "arg": produce_type})
+		body.add_action({"obj": self, "method": "pickup_seed", "arg": body})
 
 
 func _on_Detectbox_body_exited(body:Node):
 	if body.has_method("remove_action"):
-		body.remove_action({"obj": body, "method": "pickup_seed", "arg": produce_type})
+		body.remove_action({"obj": self, "method": "pickup_seed", "arg": body})
+
+##########################################################
+# animate
+
+var max_dir_distance = 100
+var duration = 0.2
+var reset_duration = 0.2
+
+func deform(direction):
+	var deformationStrength = clamp(max_dir_distance - direction.length(), 0, max_dir_distance) / max_dir_distance
+	var deformationDirection = direction.normalized()
+	var deformationScale = 0.5 * deformationDirection * deformationStrength
+
+	var tween = create_tween()
+	tween.tween_property(anim.material, "shader_param/deformation",
+		deformationScale, duration).set_trans(Tween.TRANS_CUBIC)
+	tween.parallel().tween_property(produce_icon.material, "shader_param/deformation",
+		deformationScale, duration).set_trans(Tween.TRANS_CUBIC)
+	tween.tween_property(anim.material, "shader_param/deformation",
+		Vector2.ZERO, reset_duration).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
+	tween.parallel().tween_property(produce_icon.material, "shader_param/deformation",
+		Vector2.ZERO, reset_duration).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
+
+##########################################################
+# interactions
+
+func pickup_seed(player):
+	player.pickup_seed(produce_type)
+	var dir = anim.global_position - player.get_global_position()
+	deform(dir)
