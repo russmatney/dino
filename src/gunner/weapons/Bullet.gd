e9fe08e72c336a78686c16aeae921646fe418704
Russell Matney <russell.matney@gmail.com>
2023-01-16 11:47:55 -0500

    fix: more impactful bullet shake
    the 0.2 is imperceptible, this turns it up a notch.

--- src/gunner/weapons/Bullet.gd
+++ src/gunner/weapons/Bullet.gd
@@ -17,7 +17,7 @@ func kill():
 	# no need to run more than once (if we contact multiple objs)
 	if not dying:
 		dying = true
-		Cam.screenshake(0.2)
+		Cam.screenshake(0.3)
 		Gunner.play_sound("bullet_pop")
 		anim.set_visible(false)
 		pop.set_visible(true)
