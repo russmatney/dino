5937fa806cf4ddb4b254cb8e305af3767af4394e
Russell Matney <russell.matney@gmail.com>
2023-01-18 11:24:15 -0500

    chore: rename force-reload, reload-scene

--- addons/core/plugin.gd
+++ addons/core/plugin.gd
@@ -2,7 +2,7 @@ tool
 extends EditorPlugin
 
 
-var force_reload_btn = Button.new()
+var reload_scene_btn = Button.new()
 var container = 0
 var editor_interface
 
@@ -12,25 +12,19 @@ func _enter_tree():
 	add_autoload_singleton("Util", "res://addons/core/Util.gd")
 
 	editor_interface = get_editor_interface()
-	force_reload_btn.connect("pressed", self, "_on_button_pressed")
-	force_reload_btn.text = "Force Reload"
-	add_control_to_container(container, force_reload_btn)
+	reload_scene_btn.connect("pressed", self, "reload_scene")
+	reload_scene_btn.text = "Reload Scene"
+	add_control_to_container(container, reload_scene_btn)
 
 
 func _exit_tree():
-
 	remove_autoload_singleton("Util")
 	print("</Core>")
 
 
-func _on_button_pressed():
-	# reopen_scene()
-	reload_scene()
-
-
 func reload_scene():
 	print("\n-----------------")
-	print("[ForceReloadEditor] reloading scene: ", Time.get_time_string_from_system())
+	print("[ReloadScene] ", Time.get_time_string_from_system())
 
 	var edited_scene = editor_interface.get_edited_scene_root()
 	print("edited scene: ", edited_scene, " filename: ", edited_scene.filename)
