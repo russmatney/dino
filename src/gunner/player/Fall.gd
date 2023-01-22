b7e28992d4d6c0b46437b023db145f0a4eddd39d
Russell Matney <russell.matney@gmail.com>
2023-01-22 11:55:40 -0500

    fix: fall damage based on speed, not distance

--- src/gunner/player/Fall.gd
+++ src/gunner/player/Fall.gd
@@ -1,15 +1,13 @@
 # Fall
 extends State
 
-var initial_y
-var fall_distance_shake_threshold = 90 # bit more than jump height
-var heavy_fall_distance_shake_threshold = 150
-var huge_fall_distance_shake_threshold = 400
+var fall_speed_shake_threshold = 700
+var heavy_fall_speed_shake_threshold = 900
+var very_heavy_fall_speed_shake_threshold = 1300
 
 func enter(_ctx = {}):
 	actor.anim.animation = "fall"
 	actor.can_wall_jump = false
-	initial_y = actor.global_position.y
 
 func physics_process(delta):
 	actor.velocity.y += actor.gravity * delta
@@ -22,6 +20,7 @@ func physics_process(delta):
 
 	actor.velocity.y = clamp(actor.velocity.y, actor.velocity.y, actor.max_fall_speed)
 
+	var vel_before_coll = actor.velocity
 	actor.velocity = actor.move_and_slide(actor.velocity, Vector2.UP)
 
 	if not actor.firing:
@@ -29,16 +28,17 @@ func physics_process(delta):
 
 	if actor.is_on_floor():
 		var shake = false
-		var fall_distance = actor.global_position.y - initial_y
-		if fall_distance >= huge_fall_distance_shake_threshold:
+		var fall_speed = vel_before_coll.y
+		print("fall_speed: ", fall_speed)
+		if fall_speed >= very_heavy_fall_speed_shake_threshold:
 			# TODO player flash color and drop health
 			# TODO player hurt sound
 			shake = 1.0
 			actor.take_damage(null, 1)
-		elif fall_distance >= heavy_fall_distance_shake_threshold:
+		elif fall_speed >= heavy_fall_speed_shake_threshold:
 			# TODO player flash color
 			shake = 0.6
-		elif fall_distance >= fall_distance_shake_threshold:
+		elif fall_speed >= fall_speed_shake_threshold:
 			shake = 0.3
 
 		if not actor.dead:
