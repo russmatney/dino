0d1df11babf941d53fe41c2a40e14998db8373aa
Russell Matney <russell.matney@gmail.com>
2023-02-17 17:10:01 -0500

    chore: logs for proving soldiers aren't processing

--- src/mvania19/enemies/Soldier.gd
+++ src/mvania19/enemies/Soldier.gd
@@ -7,13 +7,15 @@ const SPEED = 150.0
 const JUMP_VELOCITY = -300.0
 var GRAVITY = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-var hit_floor = false
+var log_every = 1.0
+var log_ttl = 1.0
 
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += GRAVITY * delta
-	if not hit_floor and is_on_floor():
-		hit_floor = true
-		print(name, " solider hit floor")
+	log_ttl -= delta
+	if log_ttl <= 0:
+		log_ttl = log_every
+		print("soldier is logging!")
 
 	move_and_slide()
