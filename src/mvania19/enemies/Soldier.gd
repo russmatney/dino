b0d143aabc9445b6fad2cf76cc8e20b4d7600d49
Russell Matney <russell.matney@gmail.com>
2023-02-17 17:10:24 -0500

    chore: clean up

--- src/mvania19/enemies/Soldier.gd
+++ src/mvania19/enemies/Soldier.gd
@@ -7,15 +7,8 @@ const SPEED = 150.0
 const JUMP_VELOCITY = -300.0
 var GRAVITY = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-var log_every = 1.0
-var log_ttl = 1.0
-
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += GRAVITY * delta
-	log_ttl -= delta
-	if log_ttl <= 0:
-		log_ttl = log_every
-		print("soldier is logging!")
 
 	move_and_slide()
