84ce95ec3dc685c60f6c0c5ed5b9a1b523f7912a
Russell Matney <russell.matney@gmail.com>
2023-02-22 11:18:11 -0500

    chore: update func signature for gdfxr

--- addons/gdfxr/plugin.gd
+++ addons/gdfxr/plugin.gd
@@ -9,7 +9,7 @@ var sfxr_editor: Control
 func _enter_tree():
 	import_plugin = preload("import_plugin.gd").new()
 	add_import_plugin(import_plugin)
-	
+
 	sfxr_editor = preload("editor/Editor.tscn").instantiate()
 	sfxr_editor.plugin = self
 	add_control_to_bottom_panel(sfxr_editor, "gdfxr")
@@ -19,16 +19,16 @@ func _exit_tree():
 	remove_control_from_bottom_panel(sfxr_editor)
 	sfxr_editor.queue_free()
 	sfxr_editor = null
-	
+
 	remove_import_plugin(import_plugin)
 	import_plugin = null
 
 
-func _handles(object: Variant) -> bool:
+func _handles(object: Object) -> bool:
 	return object is AudioStreamWAV and object.resource_path.ends_with(".sfxr")
 
 
-func _edit(object: Variant):
+func _edit(object: Object) -> void:
 	sfxr_editor.edit(object)
 
 
