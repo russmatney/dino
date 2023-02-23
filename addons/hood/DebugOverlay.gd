bc577de4ec4ddaf5c02a78685eba5b8c2bb68d32
Russell Matney <russell.matney@gmail.com>
2023-02-23 12:53:26 -0500

    feat: add slowmo to debug_overlay to aid debugging

--- addons/hood/DebugOverlay.gd
+++ addons/hood/DebugOverlay.gd
@@ -12,3 +12,15 @@ func _ready():
 func _unhandled_input(event):
 	if Trolley.is_debug_toggle(event):
 		set_visible(!visible)
+	elif Trolley.is_event(event, "slowmo"):
+		slowmo_start()
+	elif Trolley.is_event_released(event, "slowmo"):
+		slowmo_stop()
+
+func slowmo_start():
+	Hood.notif("Slooooooow mooooootion")
+	Cam.start_slowmo("debug_overlay_slowmo", 0.3)
+
+func slowmo_stop():
+	Hood.notif("Back to full speed")
+	Cam.stop_slowmo("debug_overlay_slowmo")
