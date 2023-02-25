769f3d47be6fd0cdced33d3b8cfca04426caf1c5
Russell Matney <russell.matney@gmail.com>
2023-02-25 14:41:02 -0500

    feat: create Debug autoload in addons/core

---
+++ addons/core/Debug.gd
@@ -0,0 +1,44 @@
+@tool
+extends Node
+
+var debugging = false
+
+signal debug_toggle(debugging)
+
+################################################
+# ready
+
+func _ready():
+	toggle_debug(debugging)
+
+################################################
+# input
+
+func _unhandled_input(event):
+	if Trolley.is_debug_toggle(event):
+		toggle_debug()
+	elif Trolley.is_event(event, "slowmo"):
+		slowmo_start()
+	elif Trolley.is_event_released(event, "slowmo"):
+		slowmo_stop()
+
+################################################
+# input
+
+func toggle_debug(d=null):
+	if d != null:
+		debugging = d
+	else:
+		debugging = !debugging
+	debug_toggle.emit(debugging)
+
+################################################
+# slowmo toggle
+
+func slowmo_start():
+	Hood.notif("Slooooooow mooooootion")
+	Cam.start_slowmo("debug_overlay_slowmo", 0.3)
+
+func slowmo_stop():
+	Hood.notif("Back to full speed")
+	Cam.stop_slowmo("debug_overlay_slowmo")
--- addons/hood/DebugOverlay.gd
+++ addons/hood/DebugOverlay.gd
@@ -1,26 +1,7 @@
 extends CanvasLayer
 
-var initial_vis = false
-# var initial_vis = true
-
-
 func _ready():
-	if not Engine.is_editor_hint():
-		set_visible(initial_vis)
+	Debug.debug_toggle.connect(_on_debug_toggle)
 
-
-func _unhandled_input(event):
-	if Trolley.is_debug_toggle(event):
-		set_visible(!visible)
-	elif Trolley.is_event(event, "slowmo"):
-		slowmo_start()
-	elif Trolley.is_event_released(event, "slowmo"):
-		slowmo_stop()
-
-func slowmo_start():
-	Hood.notif("Slooooooow mooooootion")
-	Cam.start_slowmo("debug_overlay_slowmo", 0.3)
-
-func slowmo_stop():
-	Hood.notif("Back to full speed")
-	Cam.stop_slowmo("debug_overlay_slowmo")
+func _on_debug_toggle(debugging):
+	set_visible(debugging)
