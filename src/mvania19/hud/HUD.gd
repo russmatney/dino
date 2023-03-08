62ee923b18454256777c8cb7c3bc1347a255581a
Russell Matney <russell.matney@gmail.com>
2023-03-08 09:20:33 -0500

    feat: add health to hud
    Wired up with hotel signals. Not 100% on this pattern - hotel's state
    _could_ get out of sync with the player... but it really shouldn't.

--- src/mvania19/hud/HUD.gd
+++ src/mvania19/hud/HUD.gd
@@ -6,11 +6,23 @@ extends CanvasLayer
 # ready
 
 func _ready():
-	Debug.prn("ready")
+	Hotel.entry_updated.connect(_on_entry_updated)
 
+func _on_entry_updated(entry):
+	# maybe strange to do this way... or maybe it's nice and decoupled?
+	if "player" in entry["groups"]:
+		set_health(entry["health"])
 
+##########################################
+# health
+
+@onready var hearts = $%HeartsContainer
+
+func set_health(health):
+	hearts.h = health
 
 ##########################################
 # minimap
 
 # @onready var minimap = $%Minimap
+
