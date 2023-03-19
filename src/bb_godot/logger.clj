9883635661d60190e565531abb598fbe9f8a89cc
Russell Matney <russell.matney@gmail.com>
2023-03-19 14:21:02 -0400

    feat: add logger/log macro for bb commands

---
+++ src/bb_godot/logger.clj
@@ -0,0 +1,17 @@
+(ns bb-godot.logger)
+
+;; https://gist.github.com/borkdude/c97da85da67c7bcc5671765aef5a89ad
+
+(defmacro log [& msgs]
+  (let [m    (meta &form)
+        _ns  (ns-name *ns*) ;; can also be used for logging
+        file *file*]
+    `(binding [*out* *err*] ;; or bind to (io/writer log-file)
+       (println (str ~file ":"
+                     ~(:line m) ":"
+                     ~(:column m))
+                ~@msgs))))
+
+;; (ns bar (:require bb-godot.logger))
+
+;; (logger/log "what goes on here")
