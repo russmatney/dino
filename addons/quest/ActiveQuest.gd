f92cbc6fc45d706e6fab6b87a41257c1f354c467
Russell Matney <russell.matney@gmail.com>
2023-05-04 16:39:44 -0400

    feat: add pretty print to ActiveQuest node
    This could use a better name! Maybe just QuestNode or QuestObj?

--- addons/quest/ActiveQuest.gd
+++ addons/quest/ActiveQuest.gd
@@ -10,3 +10,15 @@ var remaining: int
 var total: int
 var optional: bool
 var check_not_failed: bool
+
+func _to_string():
+	return Debug.to_pretty({
+		label=label,
+		complete=complete,
+		failed=failed,
+		node=node,
+		remaining=remaining,
+		total=total,
+		optional=optional,
+		check_not_failed=check_not_failed,
+		})
