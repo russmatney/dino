da02d3258b1dcc702f0661d560f984ac331edaa1
Russell Matney <russell.matney@gmail.com>
2023-05-05 13:32:30 -0400

    fix: initial hud player state

--- src/herd/hud/HUD.gd
+++ src/herd/hud/HUD.gd
@@ -3,6 +3,7 @@ extends CanvasLayer
 
 func _ready():
 	Hotel.entry_updated.connect(_on_entry_updated)
+	_on_entry_updated(Hotel.first({is_player=true}))
 
 func _on_entry_updated(entry):
 	if "player" in entry.get("groups", []):
