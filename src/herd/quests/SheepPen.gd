36b89cdcebb6e3ff5f763962ab1f0b36c0c6f38b
Russell Matney <russell.matney@gmail.com>
2023-05-06 13:54:03 -0400

    feat: add green box to sheep pen collision shape
    Could pull a util out of this for re-use at some point.
    
    Would also be nice to add color to arbitrary collision shapes.

---
+++ src/herd/quests/SheepPen.gd
@@ -0,0 +1,25 @@
+@tool
+extends Area2D
+
+@onready var coll: CollisionShape2D = $CollisionShape2D
+
+func _ready():
+	add_color_rect(coll)
+
+func add_color_rect(coll_shape):
+	for ch in get_children():
+		if ch is ColorRect:
+			ch.free()
+
+	if not coll_shape:
+		return
+
+	var shape_rect = coll_shape.shape.get_rect()
+	var color_rect = ColorRect.new()
+
+	color_rect.position = shape_rect.position + coll.position
+	color_rect.size = shape_rect.size
+	color_rect.color = Color(.5, .8, .5, 0.4)
+
+	add_child(color_rect)
+	color_rect.set_owner(self)
