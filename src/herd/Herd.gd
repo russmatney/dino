4f7fd541484bc8508309a55ba00a9a3e9c1d42c1
Russell Matney <russell.matney@gmail.com>
2023-05-06 21:15:59 -0400

    feat: some tut levels and then a...rearrangement

--- src/herd/Herd.gd
+++ src/herd/Herd.gd
@@ -26,7 +26,10 @@ func register():
 
 var levels = [
 	"res://src/herd/levels/LevelOne.tscn",
-	"res://src/herd/levels/LevelTwo.tscn"
+	"res://src/herd/levels/LevelThrowSheep.tscn",
+	"res://src/herd/levels/LevelPlayerJump.tscn",
+	"res://src/herd/levels/LevelTwo.tscn",
+	"res://src/herd/levels/LevelThree.tscn"
 	]
 
 var level_idx = 0
