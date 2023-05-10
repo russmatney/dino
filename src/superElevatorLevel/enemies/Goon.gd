c4684c35644064aac1a8a926d509c949cf111884
Russell Matney <russell.matney@gmail.com>
2023-05-09 21:17:55 -0400

    wip: rudimentary goon ai

--- src/superElevatorLevel/enemies/Goon.gd
+++ src/superElevatorLevel/enemies/Goon.gd
@@ -1,2 +1,13 @@
 @tool
 extends BEUBody
+
+
+## physics ###########################################################
+
+func _physics_process(delta):
+	super._physics_process(delta)
+
+	# very basic enemy fighting back
+	# needs delete and better effort
+	if len(punch_box_bodies) > 0 and machine.state.name in ["Idle", "Wander"]:
+		machine.transit("Punch")
