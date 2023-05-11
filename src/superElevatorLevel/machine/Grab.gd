f981e91f7775c1876109f87a06cdc2e632d350f5
Russell Matney <russell.matney@gmail.com>
2023-05-11 11:38:43 -0400

    refactor: slight grab state cleanup

--- src/superElevatorLevel/machine/Grab.gd
+++ src/superElevatorLevel/machine/Grab.gd
@@ -5,33 +5,32 @@ var grabbed
 var grab_time = 0.8
 var grab_ttl
 
+var throw_direction
+
 ## enter ###########################################################
 
 func enter(opts = {}):
 	grabbed = opts.get("grabbed")
 	grab_ttl = grab_time
 
+	if grabbed.global_position.x > actor.global_position.x:
+		throw_direction = Vector2.LEFT
+	elif grabbed.global_position.x < actor.global_position.x:
+		throw_direction = Vector2.RIGHT
 
 ## exit ###########################################################
 
 func exit():
 	grabbed = null
+	throw_direction = null
 
 
 ## input ###########################################################
 
 func unhandled_input(event):
 	if Trolley.is_attack(event):
-		var direction = Vector2.UP
-		if grabbed.global_position.x > actor.global_position.x:
-			direction = Vector2.LEFT
-		elif grabbed.global_position.x < actor.global_position.x:
-			direction = Vector2.RIGHT
-
-		grabbed.machine.transit("Thrown", {thrown_by=actor, direction=direction})
+		grabbed.machine.transit("Thrown", {thrown_by=actor, direction=throw_direction})
 		transit("Throw", {body=grabbed})
-
-		return
 
 ## physics ###########################################################
 
