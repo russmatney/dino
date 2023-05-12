ffd2bd98271ddd80ff9d6afb6efe4d7d9b08f5ce
Russell Matney <russell.matney@gmail.com>
2023-05-12 15:55:03 -0400

    feat: tweak floor_count, add elevating sound

--- src/superElevatorLevel/levels/Elevator.gd
+++ src/superElevatorLevel/levels/Elevator.gd
@@ -1,23 +1,26 @@
 extends SELLevel
 
 var waves = [
-	{goon_count=1},
-	{goon_count=3, screenshake=0.6, animation_loops=3},
-	{goon_count=2},
-	{goon_count=4, screenshake=0.5, animation_loops=4, animation_time=0.5}
+	{goon_count=1, floor_count=3},
+	{goon_count=3, floor_count=8, screenshake=0.6, animation_loops=4},
+	{goon_count=2, floor_count=2, animation_loops=2},
+	{goon_count=4, floor_count=7, screenshake=0.5, animation_loops=4, animation_time=1.0},
+	{goon_count=1, floor_count=12},
 	]
 
 func _ready():
 	super._ready()
 
-var floor_num = 0
+var floor_num = 1
 
 func spawn_next_wave(wave):
-	floor_num += 1
+	floor_num += wave.get("floor_count", 1)
 	Hood.notif(str("Floor ", floor_num))
 
 	var tween = create_tween()
-	tween.set_loops(wave.get("animation_loops", 2))
+	tween.set_loops(wave.get("animation_loops", 3))
+	tween.tween_callback(DJZ.play.bind(DJZ.S.fall))
+	tween.tween_callback(DJZ.play.bind(DJZ.S.jet_init))
 	tween.tween_callback(Cam.screenshake.bind(wave.get("screenshake", 0.4))).set_delay(0.5)
 
 	await get_tree().create_timer(wave.get("animation_time", 2.0)).timeout
