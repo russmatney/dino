7a2dea3538121111b1eedf1f7f300d668d601dc4
Russell Matney <russell.matney@gmail.com>
2023-05-12 15:48:52 -0400

    feat: super elevator level infers level_idx

--- src/superElevatorLevel/SuperElevatorLevel.gd
+++ src/superElevatorLevel/SuperElevatorLevel.gd
@@ -5,6 +5,10 @@ extends DinoGame
 var player_scene = preload("res://src/superElevatorLevel/players/PlayerOne.tscn")
 
 func manages_scene(scene):
+	var level_paths = levels.map(func(l): return l.resource_path)
+	if scene.scene_file_path in level_paths:
+		level_idx = level_paths.find(scene.scene_file_path)
+
 	return scene.scene_file_path.begins_with("res://src/superElevatorLevel")
 
 func register():
