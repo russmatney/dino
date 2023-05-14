3eaadb05cfd2d535231ee7ac7229e67bc2f71ddc
Russell Matney <russell.matney@gmail.com>
2023-05-14 13:40:47 -0400

    feat: palette swaps on boss character

--- src/superElevatorLevel/enemies/Boss.gd
+++ src/superElevatorLevel/enemies/Boss.gd
@@ -1,3 +1,18 @@
 extends BEUBody
 
 # TODO hitstop on punches?
+
+@onready var palettes = [null,
+	preload("res://src/superElevatorLevel/enemies/BossAlt1.tres"),
+	preload("res://src/superElevatorLevel/enemies/BossAlt2.tres"),
+	]
+
+## ready ###########################################################
+
+func _ready():
+	super._ready()
+
+	var pal = Util.rand_of(palettes)
+
+	if pal != null:
+		anim.material = pal
--- src/superElevatorLevel/enemies/Goon.gd
+++ src/superElevatorLevel/enemies/Goon.gd
@@ -15,21 +15,3 @@ func _ready():
 
 	if pal != null:
 		anim.material = pal
-
-
-# func _enter_tree():
-# 	Debug.prn("id", get_instance_id())
-# 	Debug.prn("inst", instance_from_id(get_instance_id()))
-# 	Debug.prn("inst.name", instance_from_id(get_instance_id()).name)
-# 	Debug.prn("name", name)
-# 	Debug.prn(
-# 	Util.packed_scene_data(self.scene_file_path, true)
-# 		)
-
-## physics ###########################################################
-
-# func _physics_process(delta):
-# 	# very basic enemy fighting back
-# 	# needs delete and better effort
-# 	if len(punch_box_bodies) > 0 and machine.state.name in ["Idle", "Wander"]:
-# 		machine.transit("Punch")
