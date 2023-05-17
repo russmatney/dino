57a39da0e233f2e413cb61a3c103ed2c531efb64
Russell Matney <russell.matney@gmail.com>
2023-05-17 17:37:26 -0400

    wip: rough shot at some parallax backgrounds

--- src/mountain/player/Player.gd
+++ src/mountain/player/Player.gd
@@ -7,7 +7,10 @@ var hud = preload("res://src/mountain/hud/HUD.tscn")
 
 func _ready():
 	if not Engine.is_editor_hint():
-		Cam.ensure_camera({player=self, zoom_rect_min=50, zoom_margin_min=120})
+		Cam.ensure_camera({player=self,
+			# zoom_rect_min=200,
+			# zoom_margin_min=120,
+			})
 		Hood.ensure_hud(hud)
 
 	super._ready()
