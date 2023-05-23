0a30fe4e39ef005db79239c55038c6a5c72c8a9a
Russell Matney <russell.matney@gmail.com>
2023-05-23 17:48:11 -0400

    wip: more focus debugging

--- addons/navi/NaviPauseMenu.gd
+++ addons/navi/NaviPauseMenu.gd
@@ -6,3 +6,12 @@ func set_focus():
 	var chs = button_list.get_children()
 	if len(chs) > 0:
 		chs[0].grab_focus()
+
+func ready() -> void:
+	get_viewport().gui_focus_changed.connect(_on_focus_changed)
+
+func _on_focus_changed(control:Control) -> void:
+	if control != null:
+		Debug.pr("focus changed!", control.name)
+	else:
+		Debug.pr("focus lost!")
--- src/dino/DinoMenu.gd
+++ src/dino/DinoMenu.gd
@@ -9,6 +9,14 @@ func _ready():
 
 	DJ.resume_menu_song()
 
+	get_viewport().gui_focus_changed.connect(_on_focus_changed)
+
 	var chs = games_list.get_children()
 	if len(chs) > 0:
 		chs[0].grab_focus()
+
+func _on_focus_changed(control:Control) -> void:
+	if control != null:
+		Debug.pr("focus changed!", control.name)
+	else:
+		Debug.pr("focus lost!")
