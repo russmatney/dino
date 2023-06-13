b52141fb6ca9f764cd45ffb2d60aab1e6bc3f04f
Russell Matney <russell.matney@gmail.com>
2023-06-13 18:11:55 -0400

    feat: topdown player controller basic walking

--- addons/beehive/topdown/machine/Idle.gd
+++ addons/beehive/topdown/machine/Idle.gd
@@ -3,8 +3,17 @@ extends State
 ## enter ###########################################################
 
 func enter(_opts = {}):
-	pass
-
+	if actor.facing_vector.y > 0:
+		actor.anim.play("idle_down")
+	elif actor.facing_vector.y < 0:
+		actor.anim.play("idle_up")
+	elif actor.facing_vector.x > 0:
+		actor.anim.play("idle_right")
+	elif actor.facing_vector.x < 0:
+		# presuming anim h_flip is done elsewhere?
+		actor.anim.play("idle_right")
+	else:
+		actor.anim.play("idle_down")
 
 ## exit ###########################################################
 
@@ -27,4 +36,11 @@ func process(_delta):
 ## physics ###########################################################
 
 func physics_process(_delta):
+	if abs(actor.move_vector.length()) > 0:
+		machine.transit("Run")
+		return
+
+	# slow down
+	actor.velocity = lerp(actor.velocity, Vector2.ZERO, 0.5)
+
 	actor.move_and_slide()
--- addons/beehive/topdown/machine/Run.gd
+++ addons/beehive/topdown/machine/Run.gd
@@ -3,7 +3,17 @@ extends State
 ## enter ###########################################################
 
 func enter(_opts = {}):
-	pass
+	if actor.facing_vector.y > 0:
+		actor.anim.play("run_down")
+	elif actor.facing_vector.y < 0:
+		actor.anim.play("run_up")
+	elif actor.facing_vector.x > 0:
+		actor.anim.play("run_right")
+	elif actor.facing_vector.x < 0:
+		# presuming anim h_flip is done elsewhere?
+		actor.anim.play("run_right")
+	else:
+		actor.anim.play("run_down")
 
 
 ## exit ###########################################################
@@ -26,6 +36,15 @@ func process(_delta):
 
 ## physics ###########################################################
 
-func physics_process(_delta):
+func physics_process(delta):
+	# apply move dir or slow down
+	if actor.move_vector:
+		var new_speed = actor.run_speed * actor.move_vector * delta
+		actor.velocity = lerp(actor.velocity, Vector2.ONE*new_speed, 0.5)
+	else:
+		actor.velocity = lerp(actor.velocity, Vector2.ZERO, 0.5)
+
 	actor.move_and_slide()
 
+	if abs(actor.velocity.length()) < 1:
+		machine.transit("Idle")
