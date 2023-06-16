f2fc9b2d5725cea7084786b0c11d01cd2b1ae751
Russell Matney <russell.matney@gmail.com>
2023-06-16 17:33:36 -0400

    wip: first shirt zone

--- src/shirt/Shirt.gd
+++ src/shirt/Shirt.gd
@@ -1,7 +1,54 @@
 @tool
 extends DinoGame
 
-var player_scene = preload("res://src/shirt/player/Player.tscn")
 
 func manages_scene(scene):
 	return scene.scene_file_path.begins_with("res://src/shirt")
+
+
+## player ##########################################################
+
+var player_scene = preload("res://src/shirt/player/Player.tscn")
+
+func get_spawn_coords():
+	# TODO consider non-global usage of Metro here
+	return Metro.get_spawn_coords()
+
+## zones ##########################################################
+
+const zone_scenes = [
+	"res://src/shirt/zones/CaveOne.tscn",
+	]
+
+## register ##########################################################
+
+var first_zone
+
+func register():
+	register_menus()
+
+	Debug.pr("Registering HatBot Zones")
+	Hotel.add_root_group(Metro.zones_group)
+
+	for sfp in zone_scenes:
+		Hotel.book(sfp)
+
+	if first_zone == null:
+		first_zone = zone_scenes[0]
+
+	var zones = Hotel.query({"group": Metro.zones_group})
+
+	Debug.pr("Shirt registered", len(zones), "zones and first zone ", first_zone)
+
+## start ##########################################################
+
+func start():
+	Debug.prn("Starting HatBot!")
+
+	# TODO pull from saved game?
+	Metro.load_zone(first_zone)
+
+## Called to trigger a world update after the player is loaded or removed
+func update_world():
+	# TODO consider non-global usage of Metro here
+	Metro.update_zone()
