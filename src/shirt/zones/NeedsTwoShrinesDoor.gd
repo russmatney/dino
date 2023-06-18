7eb6940791337455ee4ef41b33ce30d8f63f26d5
Russell Matney <russell.matney@gmail.com>
2023-06-18 08:45:06 -0400

    feat: show message when not enough shrine gems

--- src/shirt/zones/NeedsTwoShrinesDoor.gd
+++ src/shirt/zones/NeedsTwoShrinesDoor.gd
@@ -9,5 +9,11 @@ func _ready():
 			source_can_execute=func(): return state == door_state.CLOSED,
 			actor_can_execute=func(actor): return actor.has_shrine_gems(2),
 			}),
+		Action.mk({
+			label="Not enough shrine gems",
+			input_action="",
+			source_can_execute=func(): return state == door_state.CLOSED,
+			actor_can_execute=func(actor): return actor.has_shrine_gems(1) or actor.has_shrine_gems(0),
+			}),
 		]
 	super._ready()
