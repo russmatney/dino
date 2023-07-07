46992f03a850ced6065201174f16ea8e1afa9716
Russell Matney <russell.matney@gmail.com>
2023-07-07 15:59:48 -0400

    feat: basic blob pickup anim and art

--- src/spike/entities/BlobPickup.gd
+++ src/spike/entities/BlobPickup.gd
@@ -7,6 +7,17 @@ var pickup_type = "blob"
 func _ready():
 	pickup_box.body_entered.connect(_on_body_entered)
 
+
+	floaty_tween()
+
+func floaty_tween():
+	var t = create_tween()
+	t.set_loops(0)
+	var og_position = position
+
+	t.tween_property(self, "position", og_position + Vector2.UP * 5.0, 0.4).set_trans(Tween.TRANS_BOUNCE)
+	t.tween_property(self, "position", og_position, 0.3).set_trans(Tween.TRANS_BOUNCE)
+
 func kill():
 	DJZ.play(DJZ.S.pickup)
 	# TODO animate
