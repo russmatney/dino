537387fb0e0b62b0a072c000b0e5e77cb036e5bd
Russell Matney <russell.matney@gmail.com>
2023-07-07 12:52:25 -0400

    fix: player group and collision layer
    Really ought to do this automagically in the ssplayer.gd

--- src/spike/player/Player.gd
+++ src/spike/player/Player.gd
@@ -1,14 +1,26 @@
 extends SSPlayer
 
+@export var zoom_rect_min = 100
+@export var zoom_margin_min = 100
+
 ## ready ##################################################################
 
 var hud = preload("res://src/spike/hud/HUD.tscn")
 
 func _ready():
 	if not Engine.is_editor_hint():
-		Cam.ensure_camera({player=self,
-			# zoom_rect_min=200,
-			# zoom_margin_min=120,
-			})
+		Cam.ensure_camera({player=self, zoom_rect_min=zoom_rect_min, zoom_margin_min=zoom_margin_min})
 		Hood.ensure_hud(hud)
+
 	super._ready()
+
+## hotel ##################################################################
+
+# func check_out(data):
+# 	super.check_out(data)
+# 	shrine_gems = Util.get_(data, "shrine_gems", shrine_gems)
+
+# func hotel_data():
+# 	var d = super.hotel_data()
+# 	d["shrine_gems"] = shrine_gems
+# 	return d
