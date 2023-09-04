6fac33d0297765e659ac3ac3b312a419a8945899
Russell Matney <russell.matney@gmail.com>
2023-09-04 17:53:10 -0400

    wip: pandora for dino games, sel bodies

--- pandora/dino_game_ids.gd
+++ pandora/dino_game_ids.gd
@@ -1,6 +1,7 @@
 # Do not modify! Auto-generated file.
-class_name DinoGameIds 
+class_name DinoGameIds
 
 
 const HATBOT = "2"
 const SUPERELEVATORLEVEL = "3"
+const SHIRT = "hu92GIyC74"
---
+++ pandora/sel_body_ids.gd
@@ -0,0 +1,7 @@
+# Do not modify! Auto-generated file.
+class_name SelBodyIds
+
+
+const SKATEALIKE = "fLJuER49T_"
+const SUITGOON = "PXu9LkIy6C"
+const BOSSTODON = "UV_zyDUdiR"
