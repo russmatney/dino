f2c09c277bd2a7d73b39b505728b9428fdf80b2e
Russell Matney <russell.matney@gmail.com>
2023-10-08 16:45:46 -0400

    feat: basic hud/anim for player

--- src/spike/player/Player.gd
+++ src/spike/player/Player.gd
@@ -1,3 +1,4 @@
+@tool
 extends SSPlayer
 
 func _get_configuration_warnings():
--- src/woods/player/WoodsPlayer.gd
+++ src/woods/player/WoodsPlayer.gd
@@ -1,2 +1,19 @@
 @tool
 extends SSPlayer
+
+func _get_configuration_warnings():
+	return super._get_configuration_warnings()
+
+@export var zoom_rect_min = 200
+@export var zoom_margin_min = 100
+
+## ready ##################################################################
+
+var hud = preload("res://src/woods/hud/WoodsHUD.tscn")
+
+func _ready():
+	if not Engine.is_editor_hint():
+		Cam.ensure_camera({player=self, zoom_rect_min=zoom_rect_min, zoom_margin_min=zoom_margin_min})
+		Hood.ensure_hud(hud)
+
+	super._ready()
