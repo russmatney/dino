aac9df7a6f8995b2efcd5fcddc0864ca93fbeb05
Russell Matney <russell.matney@gmail.com>
2023-10-08 17:07:36 -0400

    feat: basic woods main/pause menus, start() impl
    Just runs the worldgen scene for now, which can be regened on the fly
    via shift-r

--- src/woods/Woods.gd
+++ src/woods/Woods.gd
@@ -1,5 +1,11 @@
 @tool
 extends DinoGame
 
+var game_scene = preload("res://src/woods/world/WorldGen.tscn")
+
 func start(opts={}):
 	Debug.pr("starting The Woods", opts)
+
+	var game = game_scene.instantiate()
+	# TODO apply options/gen level
+	Navi.nav_to(game)
