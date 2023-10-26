eca0743a7ba2b35f5bcd18863cda1c7ca7c26cd0
Russell Matney <russell.matney@gmail.com>
2023-10-25 20:19:34 -0400

    feat: point to levelGen for playable pluggs scene

--- src/pluggs/Pluggs.gd
+++ src/pluggs/Pluggs.gd
@@ -1,7 +1,8 @@
 @tool
 extends DinoGame
 
-var first_scene = "res://src/pluggs/player/PluggsGym.tscn"
+# var first_scene = "res://src/pluggs/player/PluggsGym.tscn"
+var first_scene = "res://src/pluggs/room/LevelGen.tscn"
 
 func start(_opts={}):
 	Navi.nav_to(first_scene)
