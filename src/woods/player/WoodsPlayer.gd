11a71ad588504cedf0f8c1d69ee047dde71ec278
Russell Matney <russell.matney@gmail.com>
2023-11-11 19:19:17 -0500

    misc: tweak some woods levels

--- src/woods/player/WoodsPlayer.gd
+++ src/woods/player/WoodsPlayer.gd
@@ -1,18 +1,21 @@
 @tool
 extends SSPlayer
 
+## warn ##################################################################
+
 func _get_configuration_warnings():
 	return super._get_configuration_warnings()
+
+## vars ##################################################################
 
 @export var zoom_rect_min = 200
 @export var zoom_margin_min = 100
 
-## ready ##################################################################
-
 var hud = preload("res://src/woods/hud/WoodsHUD.tscn")
 
+## ready ##################################################################
+
 func _ready():
-	Log.pr("woods player ready")
 	if not Engine.is_editor_hint():
 		Cam.request_camera({player=self, zoom_rect_min=zoom_rect_min, zoom_margin_min=zoom_margin_min})
 		Hood.ensure_hud(hud)
@@ -21,18 +24,7 @@ func _ready():
 
 ## collect ##################################################################
 
-func collect(entity: WoodsEntity, _opts={}):
-	match entity.type:
-		WoodsEntity.t.Leaf: Log.pr("player collected leaf")
-
-## reach end ##################################################################
-
-func entered_end_box():
-	Log.pr("Player entered end box!")
-
-	var cs = Navi.current_scene
-	if cs.has_method("generate"):
-		Log.pr("quick n dirty replay")
-		cs.generate()
-		Game.respawn_player()
-		return
+func collect(_entity: WoodsEntity, _opts={}):
+	pass
+	# match entity.type:
+	# 	WoodsEntity.t.Leaf: Log.pr("player collected leaf")
