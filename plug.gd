8f6fa9cdca248c46abfff984e76cea52db67f25d
Russell Matney <russell.matney@gmail.com>
2024-03-21 19:53:28 -0400

    feat: install gd-plug

---
+++ plug.gd
@@ -0,0 +1,12 @@
+extends "res://addons/gd-plug/plug.gd"
+
+func _plugging():
+	plug("MikeSchulze/gdUnit4", {include=["addons/gdUnit4"]})
+	plug("imjp94/gd-plug-ui")
+	plug("bitbrain/pandora", {include=["addons/pandora"]})
+	plug("nathanhoad/godot_input_helper", {include=["addons/input_helper"]})
+	plug("nathanhoad/godot_sound_manager", {include=["addons/sound_manager"]})
+	plug("nathanhoad/godot_dialogue_manager", {include=["addons/dialogue_manager"]})
+	plug("timothyqiu/gdfxr")
+	plug("viniciusgerevini/godot-aseprite-wizard", {include=["addons/AsepriteWizard"]})
+	plug("russmatney/log.gd", {include=["addons/log"]})
