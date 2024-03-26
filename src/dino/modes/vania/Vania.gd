793987c8940e852d3041ab4d4ceb2aadcd043ce4
Russell Matney <russell.matney@gmail.com>
2024-03-26 19:32:55 -0400

    wip: ugly, but finally generating rooms in metsys

--- src/dino/modes/vania/Vania.gd
+++ src/dino/modes/vania/Vania.gd
@@ -50,19 +50,16 @@ func start_game():
 		game_node.queue_free()
 
 	# setup level
-	var level_def = Pandora.get_entity(LevelDefIds.BOSSBATTLE)
-	var level_opts = {seed=_seed, skip_splash_intro=true}
-	var level_node = DinoLevel.create_level(level_def, level_opts)
+	# var level_def = Pandora.get_entity(LevelDefIds.BOSSBATTLE)
+	# var level_opts = {seed=_seed, skip_splash_intro=true}
+	# var level_node = DinoLevel.create_level(level_def, level_opts)
 
-	Log.pr("level node", level_node)
-	Log.pr("level node get spawn point", U.first_node_in_group(level_node, "player_spawn_points"))
+	# Dino.spawn_player({level_node=level_node, deferred=false})
 
-	Dino.spawn_player({level_node=level_node, deferred=false})
-
-	level_node.level_complete.connect(_on_level_complete)
+	# level_node.level_complete.connect(_on_level_complete)
 
 	game_node = vania_game_scene.instantiate()
-	game_node.add_level(level_node, level_def, level_opts)
+	# game_node.add_level(level_node, level_def, level_opts)
 
 	add_child.call_deferred(game_node)
 
--- src/dino/modes/vania/VaniaGame.gd
+++ src/dino/modes/vania/VaniaGame.gd
@@ -1,30 +1,49 @@
 extends "res://addons/MetroidvaniaSystem/Template/Scripts/MetSysGame.gd"
 class_name VaniaGame
 
+const GEN_MAP_DIR = "user://vania_maps"
+const MAP_DIR = "res://src/dino/modes/vania/maps"
 var starting_map: String = "VaniaMap.tscn"
+var new_generated_maps = []
+
+var generating: Thread
+
+# func to_printable():
+# 	return {level_def=level_def.get_display_name()}
 
 func _ready():
 	MetSys.reset_state()
 	MetSys.set_save_data()
+
+	generate_map()
+
+	room_loaded.connect(init_room, CONNECT_DEFERRED)
+	if new_generated_maps.is_empty():
+		load_room(starting_map)
+	else:
+		load_room(new_generated_maps[0])
+
+	Dino.spawn_player({level_node=self, deferred=false})
 
 	var p = Dino.current_player_node()
 	Log.pr("current player node", p)
 	if p:
 		set_player(p)
 
-	room_loaded.connect(init_room, CONNECT_DEFERRED)
-	load_room(starting_map)
-
-	# var start := map.get_node_or_null(^"SavePoint")
-	# if start:
-	# 	player.position = start.position
-
 	add_module.call_deferred("RoomTransitions.gd")
 
 func init_room():
-	Log.pr("room entered")
+	Log.pr("room entered", MetSys.get_current_room_instance())
+	Log.pr("map ", MetSys.get_save_data())
+	Log.pr("map data ", MetSys.map_data)
+	Log.pr("map data cells", MetSys.map_data.cells)
+	Log.pr("map data assigned scenes", MetSys.map_data.assigned_scenes)
+	Log.pr("player corods", MetSys.get_current_coords())
+	Log.pr("this room's cells", MetSys.get_current_room_instance().get_local_cells())
 	# MetSys.get_current_room_instance().adjust_camera_limits($Player/Camera2D)
 	# player.on_enter()
+
+## dino level ##########################################################
 
 var level_def: LevelDef
 var level_node
@@ -36,3 +55,56 @@ func add_level(node, def, opts):
 	level_opts = opts
 
 	add_child(level_node)
+
+## generate_map ##########################################################
+
+var map_coords = [
+	Vector3i(0, 0, 0),
+	Vector3i(1, 0, 0),
+	Vector3i(2, 0, 0),
+	Vector3i(2, 1, 0),
+	Vector3i(1, 1, 0),
+	]
+
+func generate_map():
+	DirAccess.make_dir_absolute(GEN_MAP_DIR)
+	for file in DirAccess.get_files_at(GEN_MAP_DIR):
+		DirAccess.remove_absolute(GEN_MAP_DIR.path_join(file))
+
+	var builder := MetSys.get_map_builder()
+
+	for coord in map_coords:
+		var cell := builder.create_cell(coord)
+		cell.color = Color.BLACK
+		for i in 4:
+			cell.borders[i] = 0
+			cell.border_colors[i] = Color.WHITE
+
+		var sc = generate_scene() # TODO probably want to pass room opts in here
+		cell.set_assigned_scene(sc)
+
+	builder.update_map()
+
+func generate_scene() -> String:
+	var map_scene: PackedScene = load("%s/%s" % [MAP_DIR, starting_map])
+
+	var scene_basename = starting_map.get_basename()
+	var new_map: String
+
+	if new_generated_maps.is_empty():
+		new_map = scene_basename + "1.tscn"
+	else:
+		new_map = "%s%d.tscn" % [scene_basename, new_generated_maps.back().to_int() + 1]
+
+	var new_map_path = GEN_MAP_DIR.path_join(new_map)
+	new_generated_maps.append(new_map_path)
+
+	# Prepare the actual scene (maybe deferred if threading)
+	var map_node: Node2D = map_scene.instantiate()
+
+	# do things like hide/show exits based on nodes and room opts
+
+	var ps := PackedScene.new()
+	ps.pack(map_node)
+	ResourceSaver.save(ps, new_map_path)
+	return new_map_path
