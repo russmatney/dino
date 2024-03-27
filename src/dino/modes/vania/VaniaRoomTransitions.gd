6ae55b77bbfd51e0b7c263c11067e6839cc87d25
Russell Matney <russell.matney@gmail.com>
2024-03-27 16:11:27 -0400

    fix: only adjust offset in one direction
    Also drops a bunch of logs and goes for the prettier log colors

--- src/dino/modes/vania/VaniaRoomTransitions.gd
+++ src/dino/modes/vania/VaniaRoomTransitions.gd
@@ -1,7 +1,6 @@
 extends "res://addons/MetroidvaniaSystem/Template/Scripts/MetSysModule.gd"
 
 func _initialize():
-	Log.pr("vania room transitions inited")
 	MetSys.room_changed.connect(_on_room_changed, CONNECT_DEFERRED)
 
 var PLAYER_POS_OFFSET = 30
@@ -11,11 +10,8 @@ func _on_room_changed(target_room: String):
 		# This can happen when teleporting to another room.
 		return
 
-	Log.pr("on room changed", target_room, MetSys.get_current_room_name())
 	var new_room_def = game.get_room_def(target_room)
 	var prev_room_def = game.get_room_def(MetSys.get_current_room_name())
-	Log.pr("new room def", new_room_def)
-	Log.pr("prev room def", prev_room_def)
 
 	var prev_room_instance := MetSys.get_current_room_instance()
 	if prev_room_instance:
@@ -25,11 +21,12 @@ func _on_room_changed(target_room: String):
 
 
 	var og_player = Dino.current_player_node()
+	Log.pr("og player velocity", og_player.velocity)
 	if (new_room_def.room_type != prev_room_def.room_type):
 		Log.pr("room type change, need to update player!")
 
 		var player_parent = og_player.get_parent()
-		# TODO carry over velocity/momentum
+		# TODO carry over velocity/momentum, plus probably other stats (health,items,etc)
 		Dino.respawn_active_player({
 			new_room_type=new_room_def.room_type, deferred=false, level_node=player_parent,
 			setup=func(p):
@@ -42,17 +39,18 @@ func _on_room_changed(target_room: String):
 			Log.warn("Room transition found no player node!")
 		else:
 			var offset = MetSys.get_current_room_instance().get_room_position_offset(prev_room_instance)
-			Log.pr("offset", offset)
 
-			if offset.y < 0:
-				offset.y += PLAYER_POS_OFFSET
-			if offset.y > 0:
-				offset.y -= PLAYER_POS_OFFSET
-			if offset.x < 0:
-				offset.x += PLAYER_POS_OFFSET
-			if offset.x > 0:
-				offset.x -= PLAYER_POS_OFFSET
+			# maybe some nice way to handle this
+			if abs(og_player.velocity.x) > abs(og_player.velocity.y):
+				if offset.x < 0:
+					offset.x += PLAYER_POS_OFFSET
+				if offset.x > 0:
+					offset.x -= PLAYER_POS_OFFSET
+			else:
+				if offset.y < 0:
+					offset.y += PLAYER_POS_OFFSET
+				if offset.y > 0:
+					offset.y -= PLAYER_POS_OFFSET
 
-			Log.pr("adjusted offset", offset)
 			player.position -= offset
 		prev_room_instance.queue_free()
