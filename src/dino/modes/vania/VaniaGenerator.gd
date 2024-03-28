2aecf0a7bcb08ca332fa7e583ae233f4d1f8c230
Russell Matney <russell.matney@gmail.com>
2024-03-28 13:04:22 -0400

    refactor: pull room_defs from dicts into VaniaRoomDef class

--- src/dino/modes/vania/VaniaGenerator.gd
+++ src/dino/modes/vania/VaniaGenerator.gd
@@ -8,18 +8,13 @@ const vania_room_wide = preload("res://src/dino/modes/vania/maps/VaniaRoomWide.t
 const vania_room_tall = preload("res://src/dino/modes/vania/maps/VaniaRoomTall.tscn")
 const vania_room_4x = preload("res://src/dino/modes/vania/maps/VaniaRoom4x.tscn")
 
-var default_cell_opts = {
-	bg_color=Color.BLACK,
-	border_color=Color.WHITE,
-	}
-
 var room_defs = [
 	{
 		room_type=DinoData.RoomType.SideScroller,
 		room_scene=vania_room,
 		coords=[Vector3i(0, 0, 0)],
 	}, {
-		room_type=DinoData.RoomType.TopDown,
+		room_type=DinoData.RoomType.SideScroller,
 		room_scene=vania_room_wide,
 		coords=[Vector3i(0, -1, 0), Vector3i(1, -1, 0),]
 	}, {
@@ -27,14 +22,14 @@ var room_defs = [
 		room_scene=vania_room_tall,
 		coords=[Vector3i(1, 0, 0), Vector3i(1, 1, 0),]
 	}, {
-		room_type=DinoData.RoomType.TopDown,
+		room_type=DinoData.RoomType.SideScroller,
 		room_scene=vania_room_4x,
 		coords=[
 			Vector3i(2, 0, 0), Vector3i(3, 0, 0),
 			Vector3i(2, 1, 0), Vector3i(3, 1, 0),
 			]
 	}
-	].map(func(opts): return U.merge(opts, default_cell_opts))
+	].map(func(opts): return VaniaRoomDef.new(opts))
 
 ## generate_rooms ##########################################################
 
@@ -47,41 +42,38 @@ func generate_rooms():
 		DirAccess.remove_absolute(GEN_MAP_DIR.path_join(file))
 
 	var builder := MetSys.get_map_builder()
-	var room_paths = []
 
-	for opts in room_defs:
-		var coords = opts.get("coords")
-		var new_room_path
-
-		for coord in coords:
+	for room_def in room_defs:
+		for index in range(len(room_def.coords)):
+			var coord = room_def.coords[index]
 			var cell := builder.create_cell(coord)
-			cell.color = opts.bg_color
+			cell.color = room_def.bg_color
 			for i in 4:
 				cell.borders[i] = 0
-				cell.border_colors[i] = opts.border_color
+				cell.border_colors[i] = room_def.border_color
 
-			if not new_room_path:
-				new_room_path = build_scene_name(U.merge(opts, {idx=len(room_paths)}))
-				opts["room_path"] = new_room_path
+			if not room_def.room_path:
+				room_def.index = index
+				room_def.room_path = build_scene_name(room_def)
 
-			cell.set_assigned_scene(new_room_path)
+			cell.set_assigned_scene(room_def.room_path)
 
 		# Does this need to be called before set_assigned_scene ?
-		prepare_scene(U.merge(opts, {room_path=new_room_path}))
+		prepare_scene(room_def)
 
 	builder.update_map()
 
 	return room_defs
 
-func build_scene_name(opts={}) -> String:
-	var res_path = opts.get("room_scene").resource_path
+func build_scene_name(room_def) -> String:
+	var res_path = room_def.room_scene.resource_path
 	var basename = res_path.get_file().get_basename()
-	var new_map = "%s%d.tscn" % [basename, opts.get("idx") + 1]
+	var new_map = "%s%d.tscn" % [basename, room_def.index + 1]
 	return GEN_MAP_DIR.path_join(new_map)
 
-func prepare_scene(opts={}):
+func prepare_scene(room_def):
 	# Prepare the actual scene (maybe deferred if threading)
-	var room: Node2D = opts.get("room_scene").instantiate()
+	var room: Node2D = room_def.room_scene.instantiate()
 
 	# TODO generate tiles, add entities, default doors, etc
 
@@ -89,4 +81,4 @@ func prepare_scene(opts={}):
 
 	var ps := PackedScene.new()
 	ps.pack(room)
-	ResourceSaver.save(ps, opts.get("room_path"))
+	ResourceSaver.save(ps, room_def.room_path)
---
+++ src/dino/modes/vania/VaniaRoomDef.gd
@@ -0,0 +1,22 @@
+@tool
+extends RefCounted
+class_name VaniaRoomDef
+
+var room_type: DinoData.RoomType = DinoData.RoomType.SideScroller
+var room_scene: PackedScene
+var room_path: String
+var coords: Array #[Vector3i]
+
+var bg_color: Color = Color.BLACK
+var border_color: Color = Color.WHITE
+
+var index: int
+
+func _init(opts={}):
+	room_type = opts.get("room_type", room_type)
+	# TODO create this dynamically - does it really need to be represented in the main map?
+	room_scene = opts.get("room_scene")
+	coords = opts.get("coords", [])
+
+	bg_color = opts.get("bg_color", bg_color)
+	border_color = opts.get("border_color", border_color)
