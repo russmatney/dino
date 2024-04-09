bbdc8783900d0c268b4286b551c42f65ed975b17
Russell Matney <russell.matney@gmail.com>
2024-04-09 14:55:11 -0400

    chore: drop dead ghosts/player and shaders

--- src/ghosts/player/MouseTracker.gd
+++
@@ -1,8 +0,0 @@
-extends RigidBody2D
-
-#func _input(event):
-#	if event is InputEventMouseMotion:
-#		global_position = event.position
-
-# func _process(delta):
-# 	global_position = get_global_mouse_position()
--- src/ghosts/player/PlayerMachine.gd
+++
@@ -1,7 +0,0 @@
-extends Machine
-
-
-func transit(state_name, args = {}):
-	super.transit(state_name, args)
-	# interesting, but connecting to the transitioned event instead
-	# owner.set_state_label(state_name)
