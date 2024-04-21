5a2401fc1e01d12d34f1d58036423e85da91859f
Russell Matney <russell.matney@gmail.com>
2024-04-21 16:29:52 -0400

    wip: toying with setting room inputs in the inspector

--- src/dino/vania/VaniaGame.gd
+++ src/dino/vania/VaniaGame.gd
@@ -14,7 +14,7 @@ var PassageAutomapper = "res://addons/MetroidvaniaSystem/Template/Scripts/Module
 @onready var playground: Node2D = $%LoadPlayground
 
 var room_defs: Array[VaniaRoomDef] = []
-var room_inputs = []
+@export var room_inputs: Array[RoomInputs] = []
 
 var generating: Thread
 
