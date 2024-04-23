03750e455729c201832d598a913526ca8a9d6b68
Russell Matney <russell.matney@gmail.com>
2024-04-23 16:38:34 -0400

    feat: dino gym auto-adds camera

--- src/dino/DinoGym.gd
+++ src/dino/DinoGym.gd
@@ -1,11 +1,32 @@
+@tool
 extends Node
 class_name DinoGym
+
+## vars #####################################################
 
 @export var player_entity: DinoPlayerEntity
 
 @export var genre_type: DinoData.GenreType = DinoData.GenreType.SideScroller
 
+## enter tree #####################################################
+
+func _enter_tree():
+	if get_node_or_null("Camera2D") == null:
+		var cam = Camera2D.new()
+		cam.name = "Camera2D"
+		var host = PhantomCameraHost.new()
+		host.name = "PhantomCameraHost"
+		cam.add_child(host)
+		add_child(cam)
+		cam.set_owner(self)
+		host.set_owner(self)
+
+## ready #####################################################
+
 func _ready():
+	if Engine.is_editor_hint():
+		return
+
 	if not Dino.current_player_entity():
 		if not player_entity:
 			player_entity = Pandora.get_entity(DinoPlayerEntityIds.HATBOTPLAYER)
