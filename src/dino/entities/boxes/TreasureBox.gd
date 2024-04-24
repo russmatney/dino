09eaa94ffc37fa4af95dacc9be3dd0e9320a0db1
Russell Matney <russell.matney@gmail.com>
2024-04-24 14:32:52 -0400

    feat: v1 treasure chest via actions and drops
    Adds an action area and open action that creates drops if any are
    assigned to the tresure chest.

--- src/dino/entities/boxes/TreasureBox.gd
+++ src/dino/entities/boxes/TreasureBox.gd
@@ -1,7 +1,30 @@
 extends Area2D
 
+## vars ###############################################
 
-# Called when the node enters the scene tree for the first time.
+@export var drops: Array[DropData]
+
+@onready var anim = $AnimatedSprite2D
+@onready var action_area = $ActionArea
+
+## ready ###############################################
+
 func _ready():
-	pass # Replace with function body.
+	anim.play("idle")
+	action_area.register_actions(actions, {source=self})
 
+## actions ###############################################
+
+var actions = [
+	Action.mk({label="Treasure Chest", fn=open, show_on_actor=true,
+		source_can_execute=func(): return len(drops) > 0}),
+	]
+
+## open ###############################################
+
+func open(_actor=null):
+	Log.pr("opening treasure chest")
+	if not drops.is_empty():
+		for drop in drops:
+			DropData.add_drop(self, drop)
+	drops = []
