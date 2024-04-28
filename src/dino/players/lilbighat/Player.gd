7cf0eb2caf0ca7d1b3db8c1cb8622eb80943dc3a
Russell Matney <russell.matney@gmail.com>
2024-04-28 18:14:34 -0400

    feat: add lil-big-hat player
    from hatbot's initial release

--- src/dino/players/lilbighat/Player.gd
+++ src/dino/players/lilbighat/Player.gd
@@ -5,10 +5,7 @@ extends SSPlayer
 
 func _ready():
 	if not Engine.is_editor_hint():
-		Cam.request_camera({player=self})
-		# powerups = SSPlayer.all_powerups
-
-		died.connect(_on_player_death)
+		died.connect(func(): stamp({ttl=0})) # perma stamp
 
 	super._ready()
 
@@ -16,20 +13,3 @@ func _on_transit(state):
 	if state in ["Fall", "Run"]:
 		stamp()
 
-## death anim, jumbotron #######################################################
-
-func _on_player_death():
-	stamp({ttl=0}) # perma stamp
-
-	# is this fade conflicting with the SS dying/dead anims/tweens?
-	var t = create_tween()
-	t.tween_property(self, "modulate:a", 0.3, 1).set_trans(Tween.TRANS_CUBIC)
-	t.parallel().tween_property(light, "scale", Vector2.ZERO, 1).set_trans(Tween.TRANS_CUBIC)
-
-	# possibly we could share/re-use this, but meh, it'll probably need specific text
-	# Jumbotron.jumbo_notif({header="You died", body="Sorry about it!",
-	# 	action="close", action_label_text="Respawn",
-	# 	on_close=P.respawn_player.bind({
-	# 		setup=func(p):
-	# 		p.is_dead = false
-	# 		Hotel.check_in(p, {health=p.initial_health})})})
