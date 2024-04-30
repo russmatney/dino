a8245f6f70425e4080e6d3ec1141f7d855223e67
Russell Matney <russell.matney@gmail.com>
2024-04-30 18:04:47 -0400

    refactor: pull harvey entities into dino/ents
    Also sets z_indexs for most top down entities, restoring harvey as a
    generated dino level.

--- src/games/harvey/level/HarveyLevelGen.gd
+++ src/games/harvey/level/HarveyLevelGen.gd
@@ -24,50 +24,50 @@ func get_room_opts(opts):
 			label_to_entity={
 				"Player": {scene=load("res://src/dino/entities/PlayerSpawnPoint.tscn")},
 				"Plot": {
-					scene=load("res://src/games/harvey/plots/Plot.tscn")
+					scene=load("res://src/dino/entities/harvey_entities/Plot.tscn")
 					},
 				"CarrotSeedBox": {
-					scene=load("res://src/games/harvey/items/SeedBox.tscn"),
+					scene=load("res://src/dino/entities/harvey_entities/SeedBox.tscn"),
 					setup=func(s):
 					s.produce_type = "carrot"
 					},
 				"OnionSeedBox": {
-					scene=load("res://src/games/harvey/items/SeedBox.tscn"),
+					scene=load("res://src/dino/entities/harvey_entities/SeedBox.tscn"),
 					setup=func(s):
 					s.produce_type = "onion"
 					},
 				"TomatoSeedBox": {
-					scene=load("res://src/games/harvey/items/SeedBox.tscn"),
+					scene=load("res://src/dino/entities/harvey_entities/SeedBox.tscn"),
 					setup=func(s):
 					s.produce_type = "tomato"
 					},
 				"WateringPail": {
-					scene=load("res://src/games/harvey/items/Tool.tscn"),
+					scene=load("res://src/dino/entities/harvey_entities/Tool.tscn"),
 					setup=func(t): t.tool_type = "watering-pail",
 					},
 				"UpDeliveryBox": {
-					scene=load("res://src/games/harvey/items/DeliveryBox.tscn"),
+					scene=load("res://src/dino/entities/harvey_entities/DeliveryBox.tscn"),
 					setup=func(b):
 					b.position += Vector2.DOWN * opts.tile_size
 					b.position += Vector2.RIGHT * opts.tile_size
 					b.rotation = PI
 					},
 				"DownDeliveryBox": {
-					scene=load("res://src/games/harvey/items/DeliveryBox.tscn"),
+					scene=load("res://src/dino/entities/harvey_entities/DeliveryBox.tscn"),
 					},
 				"LeftDeliveryBox": {
-					scene=load("res://src/games/harvey/items/DeliveryBox.tscn"),
+					scene=load("res://src/dino/entities/harvey_entities/DeliveryBox.tscn"),
 					setup=func(b):
 					b.position += Vector2.RIGHT * opts.tile_size
 					b.rotation = PI/2
 					},
 				"RightDeliveryBox": {
-					scene=load("res://src/games/harvey/items/DeliveryBox.tscn"),
+					scene=load("res://src/dino/entities/harvey_entities/DeliveryBox.tscn"),
 					setup=func(b):
 					b.position += Vector2.DOWN * opts.tile_size
 					b.rotation = 3*PI/2
 					},
-				"Bot": {scene=load("res://src/games/harvey/bots/HarveyBot.tscn")},
+				"Bot": {scene=load("res://src/dino/npcs/action_bot/HarveyBot.tscn")},
 			}})
 
 	return agg.room_opts
