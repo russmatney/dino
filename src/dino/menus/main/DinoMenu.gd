47eed652adc864822cc1f5755ce29692f62490cd
Russell Matney <russell.matney@gmail.com>
2024-05-09 20:01:56 -0400

    wip: main menu relayout, list of launch games
    These buttons don't nav to anything yet - Just whipping the main menu
    into shape for now.

--- src/dino/menus/main/DinoMenu.gd
+++ src/dino/menus/main/DinoMenu.gd
@@ -1,7 +1,20 @@
 @tool
 extends CanvasLayer
 
+## vars ##################################################################3
+
 @onready var games_grid_container = $%GamesGridContainer
+
+@onready var games_list = $%GamesList
+
+@onready var start_button = $%StartButton
+@onready var options_button = $%OptionsButton
+@onready var credits_button = $%CreditsButton
+@onready var quit_button = $%QuitButton
+
+@onready var credits_scene = preload("res://src/dino/menus/DinoCredits.tscn")
+
+## ready ##################################################################3
 
 func _ready():
 	if Engine.is_editor_hint():
@@ -9,15 +22,33 @@ func _ready():
 
 	build_games_grid()
 
+	build_games_list()
+
 	if not Engine.is_editor_hint():
 		Music.resume_menu_song()
 		set_focus()
 
-func set_focus():
-	var chs = games_grid_container.get_children()
-	if len(chs) > 0:
-		chs[0].set_focus()
+		quit_button.pressed.connect(get_tree().quit)
+		credits_button.pressed.connect(func(): Navi.nav_to(credits_scene))
 
+		start_button.pressed.connect(func():
+			Log.pr("nothing doing, start button not impled"))
+		options_button.pressed.connect(func():
+			Log.pr("nothing doing, opts button not impled"))
+
+## set_focus ##################################################################3
+
+func set_focus():
+	var chs = games_list.get_children()
+	if len(chs) > 0:
+		if chs[0].has_method("set_focus"):
+			chs[0].set_focus()
+		elif chs[0].has_method("grab_focus"):
+			chs[0].grab_focus()
+		else:
+			Log.warn("Couldn't set focus!")
+
+## game_modes grid ##################################################################3
 
 func start_mode(mode):
 	if mode:
@@ -25,11 +56,34 @@ func start_mode(mode):
 	else:
 		Log.err("Cannot start game, no game_entity passed!")
 
-var modes = []
 func build_games_grid():
-	modes = DinoModeEntity.all_modes()
 	U.free_children(games_grid_container)
 
-	for m in modes:
+	for m in DinoModeEntity.all_modes():
 		var button = EntityButton.newButton(m, start_mode)
 		games_grid_container.add_child(button)
+
+## games list ##################################################################3
+
+var games = [
+	"Classic Mode (Side-Scroller)",
+	"Classic Mode (Top Down)",
+	"Vania (Hat Bot/Ghost House/Demo Land)",
+	"Dungeon Crawler (Shirt)",
+	"Tower",
+	"Woods",
+	"Mountain",
+	"Super Elevator Level",
+	"Boss Rush",
+	"Harvey/Spike",
+	"Arcade",
+	]
+
+func build_games_list():
+	U.free_children(games_list)
+
+	for g in games:
+		var button = Button.new()
+		button.text = g
+		button.set_theme_type_variation("BlueButton")
+		games_list.add_child(button)
