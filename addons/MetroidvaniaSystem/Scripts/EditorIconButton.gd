560d68b26a091963818623995fcc0d8010761a07
Russell Matney <russell.matney@gmail.com>
2026-01-03 11:16:05 -0500

    chore: delete old metsys files
    My dep updates are pretty messy these days! These were moved, but left
    around b/c i didn't clear the existing metsys when i updated.

--- addons/MetroidvaniaSystem/Scripts/EditorIconButton.gd
+++
@@ -1,8 +0,0 @@
-@tool
-extends Button
-
-@export var icon_name: String
-
-func _notification(what: int) -> void:
-	if what == NOTIFICATION_THEME_CHANGED and not is_part_of_edited_scene():
-		icon = get_theme_icon(icon_name, &"EditorIcons")
